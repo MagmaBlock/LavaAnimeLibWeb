9488e5fb614426e876906d0594926e31472972a9
岩浆块Magma <magmablock@qq.com>
2022-08-15 21:08:26 +0800

    增加集数浮动显示，优化全部按钮在最前

--- src/components/Anime/AnimePageRight.vue
+++ src/components/Anime/AnimePageRight.vue
@@ -8,13 +8,7 @@ export default {
       epKeys: Object.keys(this.father.epVideoList).sort()
     }
   },
-  methods: {
-    whatArrayShouldIFor() {
-      // if (this.father.selectedVideoList == 'all') 
-      return this.father.videoList
-      // else return this.father.epVideoList[father.selectedVideoList]
-    }
-  }
+  methods: {}
 }
 </script>
 <template>
@@ -25,29 +19,32 @@ export default {
       <div class="text-lg">筛选集数 (Beta) <span v-if="father.selectedVideoList != 'all'" class="text-sm text-gray-600">(已选
           {{ father.selectedVideoList }})</span></div>
       <div class="flex flex-wrap py-2">
-        <div v-for="key in epKeys" class="bg-gray-100 text-blue-600 hover:bg-gray-200 text-center
-        px-2.5 py-2 lg:px-2 lg:py-1.5 mr-1 mb-1 rounded
-        ease-in duration-200" @click="father.selectedVideoList = key">
-          {{ key }}
-        </div>
         <div class="bg-gray-100 text-blue-600 hover:bg-gray-200 text-center
         px-2.5 py-2 lg:px-2 lg:py-1.5 mr-1 mb-1 rounded
         ease-in duration-200" @click="father.selectedVideoList = 'all'">
           全部
         </div>
+        <div v-for="key in epKeys" class="bg-gray-100 text-blue-600 hover:bg-gray-200 text-center
+        px-2.5 py-2 lg:px-2 lg:py-1.5 mr-1 mb-1 rounded
+        ease-in duration-200" @click="father.selectedVideoList = key">
+          {{ key }}
+        </div>
       </div>
       <!-- 视频列表，默认渲染所有资源，可通过上方的 father.selectedVideoList 控制渲染哪些集数 -->
-      <div v-for="video in this.father.epVideoList[father.selectedVideoList] || this.father.videoList" class="ease-in duration-200
-        hover:bg-zinc-200 leading rounded-md">
+      <div v-for="video in father.epVideoList[father.selectedVideoList] || father.videoList"
+        class="relative ease-in duration-200 hover:bg-zinc-200 leading rounded-md">
         <div v-if="video.type == 'file'" class="py-1">
+          <n-tag class="absolute bottom-0 right-0" v-if="father.selectedVideoList == 'all'" size="small" type="warning">
+            {{ video.episode }}
+          </n-tag>
           <!-- 发布组 -->
           <span v-for="group in video.groupNames" class="inline-flex">
             <n-tag size="small" :bordered="false" type="info" class="mx-0.5 my-0.5">
               {{ group }}
             </n-tag>
           </span>
+          <!-- 普通标签 -->
           <span v-for="tag in video.tagedName" class="inline-flex">
-            <!-- Text -->
             <span v-if="typeof tag == 'string'" class="mx-0.5">{{ tag }}</span>
             <n-tag v-if="typeof tag == 'object' && tag.type == 'source'" class="mx-0.5 my-0.5" type="success"
               size="small" :bordered="false">
@@ -66,6 +63,7 @@ export default {
               {{ tag.result }}
             </n-tag>
           </span>
+          <!-- 资源格式 -->
           <n-tag class="mx-0.5 my-0.5 inline-flex bg-gray-400 text-white font-medium" size="small" :bordered="false">
             {{ video.extensionName.result }}
           </n-tag>
