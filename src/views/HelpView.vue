ebb77b8e2a939a6cd155f7cf1c4a9cc83e384c4a
岩浆块Magma <magmablock@qq.com>
2022-08-22 19:37:04 +0800

    欸嘿? (修改开发期间没用的主页和帮助页占位)

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -1,5 +1,19 @@
 <template>
-  <Container>
-    Help
+  <Container class="grid place-items-center h-screen">
+    <n-result status="418" :title="`我只是个${hh ? '吟游诗人' : '杯具'}，又知道什么呢`"
+      :description="hh ? '欸嘿?' : '一切尽在不言中'">
+      <template #footer>
+        <n-button @click="hh = true">{{ hh ? '欸嘿?' : '接受真相就是这么简单, 这还没写好, 去看看别的吧' }}</n-button>
+      </template>
+    </n-result>
   </Container>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      hh: false
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,52 @@
 <template>
   <Container>
-      <iframe src="https://www.yuque.com/docs/share/dc3b650c-7b09-4432-b2b0-03cdc4f5ee90?view=doc_embed"
-        class="w-full h-96"></iframe>
+    <n-result status="418" title="我只是个杯具，又知道什么呢" description="一切尽在不言中">
+      <template #footer>
+        <n-button>接受真相就是这么简单</n-button>
+      </template>
+    </n-result>
+    <n-gradient-text type="info" :size="24">
+      这里应该有一个主页
+    </n-gradient-text><br>
+    <n-gradient-text type="danger" :size="18">
+      还没写呢
+    </n-gradient-text><br>
+    <n-gradient-text type="success" :size="18">
+      先定个小目标, 收录他
+      <n-number-animation :from="0" :to="100000000" /> 部番
+    </n-gradient-text>
+    <n-collapse>
+      <n-collapse-item title="可以折叠哦" name="1">
+        <div>可以哦</div>
+      </n-collapse-item>
+    </n-collapse>
+    <n-carousel show-arrow draggable class="max-w-lg my-2">
+      <img class="w-full h-60 object-cover"
+        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg">
+      <img class="w-full h-60 object-cover"
+        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg">
+      <img class="w-full h-60 object-cover"
+        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg">
+      <img class="w-full h-60 object-cover"
+        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg">
+    </n-carousel>
+    <n-switch size="medium" v-model:value="this.switch">
+      <template #icon>
+        🤔
+      </template>
+    </n-switch> 哇是{{ this.switch ? '开' : '关' }}关耶
   </Container>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      switch: true
+    }
+  }
+}
+</script>
 
 <style>
 </style>
