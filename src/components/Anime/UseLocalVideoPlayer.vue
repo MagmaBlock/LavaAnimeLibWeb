c2011e7400cf3c3de40451cf1c44b03db738c4a1
岩浆块Magma <magmablock@qq.com>
2022-09-02 12:56:50 +0800

    修复 Link.svg 大小写错误

--- src/components/Anime/UseLocalVideoPlayer.vue
+++ src/components/Anime/UseLocalVideoPlayer.vue
@@ -37,7 +37,7 @@
       <div @click="this.pausePlayer(); reporter({ type: 'Copy Link' })" :data-clipboard-text="this.video.url"
         ref="copyBtn"
         class="h-8 flex place-items-center bg-gray-100 text-blue-600 hover:bg-gray-200 active:bg-gray-300 ease-in duration-100 rounded py-1 px-2 text-xs">
-        <img src="../../assets/PlayersIcon/link.svg" alt="copy link" class="w-6 h-6">
+        <img src="../../assets/PlayersIcon/Link.svg" alt="copy link" class="w-6 h-6">
         <div class="text-gray-800 mx-2">复制链接</div>
       </div>
     </div>
