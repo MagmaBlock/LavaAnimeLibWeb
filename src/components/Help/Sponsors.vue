dcf224b9d9a811a153f89e36fa348274dbd4720e
岩浆块Magma <magmablock@qq.com>
2022-09-07 22:39:41 +0800

    优化赞助列表渲染逻辑

--- src/components/Help/Sponsors.vue
+++ src/components/Help/Sponsors.vue
@@ -5,7 +5,7 @@
       <div class="mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-2 gap-4">
         <template v-for="sponsor in sponsors">
           <SponsorCard :sponsor="sponsor" @click="sponsor.showModal = true" />
-          
+
           <n-modal v-model:show="sponsor.showModal">
             <n-card class="w-80" :title="sponsor.name" :bordered="false" role="dialog">
               <template #header-extra>
@@ -27,22 +27,29 @@
 </template>
 
 <script>
+import { ref } from 'vue';
 import SponsorCard from './SponsorCard.vue';
 export default {
   data() {
     return {
-      sponsors: [
-        { name: "Longtianmu", support: "￥240", link: "https://ltm.ink/", message: "" },
-        { name: "czy0729", support: "￥99.99", link: "https://github.com/czy0729", message: "" },
-        { name: "Arthals", support: "￥100.00", link: "", message: "" },
-        { name: "小林", support: "￥50.00", link: "", message: "" },
-        { name: "XJH_Jorhai", support: "￥35.00", link: "", message: "麦老师是我的神明呜呜呜" },
-        { name: "Arthals", support: "其他", link: "", message: "提供了数月的下载节点支持" },
-        { name: "Loliy", support: "￥13.00", link: "", message: "喝快乐水" },
+      sponsors: [],
+      sponsorsRaw: [
+        { name: "Longtianmu", support: "￥240", link: "https://ltm.ink/" },
+        { name: "czy0729", support: "￥99.99", link: "https://github.com/czy0729" },
+        { name: "Arthals", support: "￥100.00" },
+        { name: "小林", support: "￥50.00" },
+        { name: "XJH_Jorhai", support: "￥35.00", message: "麦老师是我的神明呜呜呜" },
+        { name: "Arthals", support: "其他", message: "提供了数月的下载节点支持" },
+        { name: "Loliy", support: "￥13.00", message: "喝快乐水" }
       ]
     };
   },
-  mounted() { },
+  mounted() {
+    let model = { name: '', support: '', link: '', message: '', showModal: ref(false) } // 兜底模板防 undefined
+    this.sponsorsRaw.forEach(sponsor => {
+      this.sponsors.push({ ...model, ...sponsor })
+    })
+  },
   components: { SponsorCard }
 }
 
