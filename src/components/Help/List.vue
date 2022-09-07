96a0b733f184fdfc0470722d6e937263134f22ab
岩浆块Magma <magmablock@qq.com>
2022-09-07 23:25:42 +0800

    优化帮助界面路由参数和脚本逻辑

--- src/components/Help/List.vue
+++ src/components/Help/List.vue
@@ -3,8 +3,8 @@
     <div class="text-lg font-medium">帮助列表</div>
     <div class="text-xs my-2 text-gray-600">在这里你可以获取到一些帮助</div>
     <div class="mt-4 bg-gray-100 rounded-md overflow-y-scroll">
-      <div class="text-sm py-2 px-4 font-medium rounded-md ease-in duration-100 hover:bg-gray-200 active:bg-gray-300" v-for="thisArt in articles"
-        @click="$emit('change-article', thisArt)"
+      <div class="text-sm py-2 px-4 font-medium rounded-md ease-in duration-100 hover:bg-gray-200 active:bg-gray-300"
+        v-for="thisArt in articles" @click="$emit('change-article', thisArt)"
         :class="{ 'text-blue-700 font-semibold bg-gray-200': thisArt.content == article.content }">
         {{ thisArt.title || thisArt.content.match(/.*/)[0].replaceAll('#', '') }}
       </div>
@@ -13,24 +13,13 @@
 </template>
 
 <script>
-import about from '../../assets/Help/about.md?raw';
-import why3rdplayer from '../../assets/Help/why3rdplayer.md?raw'
-import list3rdplayer from '../../assets/Help/list3rdplayer.md?raw'
 export default {
   data() {
-    return {
-      articles: [
-        { content: about },
-        { content: why3rdplayer },
-        { content: list3rdplayer }
-      ]
-    }
+    return {}
   },
   props: {
-    article: Object
-  },
-  mounted() {
-    this.$emit('change-article', { content: about })
+    article: Object,
+    articles: Object
   }
 }
 
--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -2,7 +2,7 @@
   <Container>
     <div class="lg:flex">
       <div class="lg:basis-1/4 select-none">
-        <List :article="article" @change-article="a => article = a" />
+        <List :article="article" :articles="articles" @change-article="a => changeArticle(a)" ref="List" />
         <Sponsors class="mt-8 hidden lg:block" />
       </div>
       <div class="lg:basis-3/4">
@@ -14,17 +14,38 @@
 </template>
 <script>
 import Container from '../components/Container.vue'
-import MarkdownRender from '../components/Help/MarkdownRender.vue';
 import List from '../components/Help/List.vue';
+import MarkdownRender from '../components/Help/MarkdownRender.vue';
 import Sponsors from '../components/Help/Sponsors.vue';
+
+import LavaAnimeLib from '../assets/Help/LavaAnimeLib.md?raw';
+import WhyExternalPlayer from '../assets/Help/WhyExternalPlayer.md?raw'
+import ExternalPlayerList from '../assets/Help/ExternalPlayerList.md?raw'
 export default {
   data() {
     return {
-      article: {}
+      article: {},
+      articles: [
+        { id: 'LavaAnimeLib', content: LavaAnimeLib },
+        { id: 'WhyExternalPlayer', content: WhyExternalPlayer },
+        { id: 'ExternalPlayerList', content: ExternalPlayerList }
+      ]
     };
   },
   mounted() {
     document.title = "帮助 | 熔岩番剧库 LavaAnimeLib";
+    let defaultArticle = this.$route.query.article || 'LavaAnimeLib'
+    this.articles.forEach(a => {
+      if (a.id == defaultArticle) {
+        this.changeArticle(a)
+      }
+    })
+  },
+  methods: {
+    changeArticle(a) {
+      this.article = a
+      this.$router.replace({ name: "Help", query: { article: a.id } });
+    }
   },
   watch: {},
   components: { Container, MarkdownRender, List, Sponsors }
