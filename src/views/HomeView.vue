b3ef09066b03d2bca815dc1d1ab829fe72e65ceb
岩浆块Magma <magmablock@qq.com>
2022-09-13 16:17:39 +0800

    检测到低版本浏览器时将在首页显示提示

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
   <ContainerMobileFull>
+    <div v-if="notSupport" class="bg-gray-200 p-4 text-xs lg:rounded-md lg:mb-4">
+      您当前使用的浏览器内核 {{ ua.engine.name }} {{ ua.engine.version }} 过于老旧，可能部分功能将无法正常使用. <br>
+      使用 2021 年后的浏览器以获得最佳体验, Chrome / Edge > 89, FireFox > 100, Safari > 15.6.
+    </div>
     <div class="lg:flex mb-4">
       <HeaderPictures class="lg:basis-1/3 sm:rounded-md lg:mr-4" />
       <BasicCard class="lg:basis-2/3 px-6 py-4">
@@ -13,6 +17,8 @@
 </template>
 
 <script>
+import uaParser from 'ua-parser-js';
+
 import HeaderPictures from "../components/Home/HeaderPictures.vue";
 import ContainerMobileFull from "../components/ContainerMobileFull.vue";
 import AnimeCollection from "../components/Home/Collection/AnimeCollection.vue";
@@ -20,15 +26,19 @@ import BasicCard from "../components/BasicCard.vue";
 export default {
   data() {
     return {
-      switch: true
+      switch: true,
+      ua: uaParser(),
+      notSupport: false
     };
   },
   mounted() {
     document.title = '主页 | 熔岩番剧库 LavaAnimeLib'
+    if (uaParser().engine.name = 'Blink' && parseInt(uaParser().engine.version) < 88) this.notSupport = true
   },
   components: { HeaderPictures, ContainerMobileFull, AnimeCollection, BasicCard }
 }
 </script>
 
 <style>
+
 </style>
