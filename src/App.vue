4790e7bb85bc532c2500d533d1211383ee0156fd
岩浆块Magma <magmablock@qq.com>
2022-09-13 22:28:07 +0800

    优化最大宽度为 2560(4K)，过大将显示容器防止侧栏过远

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,18 @@ export default {
 </script>
 
 <template>
-  <div class="lg:flex lg:flex-row lg:flex-nowrap h-screen">
-    <!-- 导航栏 -->
-    <NavBar />
-    <!-- 当前路由的界面 -->
-    <RouterView :memory="this.memory" :key="$route.path" class="overflow-y-auto w-full" />
+  <div class="bg-gray-200">
+    <div class="flex flex-row flex-nowrap h-screen max-w-[2560px] mx-auto shadow-lg bg-white">
+      <!-- 导航栏 -->
+      <NavBar />
+      <!-- 当前路由的界面 -->
+      <RouterView :memory="this.memory" :key="$route.path" class="overflow-y-auto w-full" />
+    </div>
   </div>
 </template>
 <!-- 从 /page/1 => /page/2, 由于这两个路由的 $route.path 并不一样, 所以组件被强制不复用。 -->
 <!-- 从 /page?id=1 => /page?id=2, 由于这两个路由的 $route.path 一样, 所以和没设置key属性一样, 会复用组件。 -->
 <!-- https://www.cnblogs.com/alyssa-1997/p/12187379.html -->
 <style>
+
 </style>
