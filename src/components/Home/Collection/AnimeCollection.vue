6775131a8d2e7aa39b55078475bdbab8b48f43f8
岩浆块Magma <magmablock@qq.com>
2022-09-15 01:08:46 +0800

    优化合并来自 Art 的主页卡片样式

--- src/components/Home/Collection/AnimeCollection.vue
+++ src/components/Home/Collection/AnimeCollection.vue
@@ -1,21 +1,20 @@
 <template>
   <div v-for="collection in collections" class="px-6 sm:px-0 py-1 sm:py-2">
-    <div class="flex items-center">
-      <div>
-        <div class="text-2xl font-semibold"> {{ collection.title }} </div>
-        <div class="h-2 w-12 bg-blue-500 rounded-full"></div>
-        <!-- <div class="text-sm text-gray-500"> {{ collection.subtitle }} </div> -->
-      </div>
-      <!--
-        后面横线，感觉观感不咋样 
-        <div class="border-4 border-gray-100 flex-grow ml-3 h-0 rounded-full">
-      </div> -->
+    <!-- 大标题块 -->
+    <div class="flex place-items-end mb-1">
+      <div class="text-xl mr-2"> {{ collection.title }} </div>
+      <div class="text-sm text-gray-500 mr-4"> {{ collection.subtitle }} </div>
+      <!-- 横线  -->
+      <div class="hidden md:inline-block flex-grow h-1 bg-gray-100 rounded"></div>
     </div>
+    <!-- 内容总块 -->
     <div class="px-2 lg:px-0">
-      <div v-for="content in collection.content">
-        <div class="flex flex-nowrap flex-row-reverse text-gray-600">
-          <div class="w-fit"> {{ content.title }} </div>
+      <!-- 单内容行块 -->
+      <div v-for="content in collection.content" class="mb-4">
+        <!-- 小标题 -->
+        <div class="flex flex-nowrap text-gray-600">
           <i class="bi bi-arrow-down-circle-fill mr-1"></i>
+          <div class="w-fit"> {{ content.title }} </div>
         </div>
         <AnimeCollectionCards :ids="content.anime" class="flex flex-shrink-0"></AnimeCollectionCards>
       </div>
--- src/components/Home/Collection/AnimeCollectionCards.vue
+++ src/components/Home/Collection/AnimeCollectionCards.vue
@@ -2,30 +2,32 @@
 <template>
   <!-- Grid 容器  -->
   <div class="my-2 grid gap-4 grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-9">
-    <!-- 真卡片链接 -->
-    <RouterLink :to="'/anime/' + anime.id" v-for="anime in data">
-      <!-- 卡片容器 -->
-      <div
-        class="relative w-full ease-in transition-all hover:shadow-lg hover:bg-blue-100 rounded-md border-gray-300 hover:border-blue-600 hover:border-2 rounder-md hover:scale-105">
+    <!-- 卡片容器 -->
+    <div class="w-full rounded-md overflow-hidden
+        ease-in transition-all hover:shadow-lg hover:border-2 hover:scale-105
+        hover:bg-blue-100 hover:border-blue-600 " v-for="anime in data">
+      <!-- 真卡片链接 -->
+      <RouterLink :to="'/anime/' + anime.id">
         <!-- 卡片图片 -->
-        <div class="overflow-hidden rounded-t-md aspect-w-2 aspect-h-3 rounded-md">
+        <div class="overflow-hidden aspect-w-2 aspect-h-3">
           <!-- 真 -->
           <img :src="anime.images.poster" alt="Poster" class="object-cover ease-in transition"
             :class="loading.img ? 'h-0 opacity-0':''">
           <!-- 骨架 -->
           <div class="w-full bg-gray-300 animate-pulse" v-if="loading.img"></div>
         </div>
-        <div class="px-3 md:px-4 py-2 rounded-b-md">
-          <n-ellipsis :line-clamp="1" expand-trigger="hover">
-            <div class="text-[14px] font-semibold leading-[18px] h-5 overflow-ellipsis break-all">{{ anime.title }}
+        <div class="px-3 md:px-4 py-2 bg-gray-100">
+          <n-ellipsis :line-clamp="2" expand-trigger="hover">
+            <div class="text-[13px] leading-[18px] h-9 break-all">
+              {{ anime.title }}
             </div>
           </n-ellipsis>
           <div class="text-xs text-gray-600">
             <i class="bi bi-play-btn"></i> {{ anime.views }}
           </div>
         </div>
-      </div>
-    </RouterLink>
+      </RouterLink>
+    </div>
   </div>
 </template>
 
