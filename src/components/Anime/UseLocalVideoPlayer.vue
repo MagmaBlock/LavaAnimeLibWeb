63d78b32acf5c5fce8df8a33c0e1c79858866a3d
岩浆块Magma <magmablock@qq.com>
2022-09-15 22:34:11 +0800

    修复PotPlayer Logo为弹弹Play的BUG

--- src/components/Anime/UseLocalVideoPlayer.vue
+++ src/components/Anime/UseLocalVideoPlayer.vue
@@ -18,7 +18,7 @@
       <!-- 其他 -->
       <a :href="getUrl().potplayer" @click="this.pausePlayer(); reporter({ type: 'PotPlayer' })"
         class="h-8 flex place-items-center bg-gray-100 text-blue-600 hover:bg-gray-200 active:bg-gray-300 ease-in duration-100 rounded py-1 px-2 text-xs">
-        <img src="../../assets/PlayersIcon/DanDanPlay.svg" alt="potplayer" class="w-6 h-6">
+        <img src="../../assets/PlayersIcon/PotPlayer.svg" alt="potplayer" class="w-6 h-6">
         <div class="text-gray-800 mx-2">PotPlayer</div>
       </a>
       <a :href="getUrl().vlc" @click="this.pausePlayer(); reporter({ type: 'VLC' })"
