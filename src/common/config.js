03b79d33b3ca5937a7fa2d4a42f61548d45d2e5b
岩浆块Magma <magmablock@qq.com>
2022-09-19 15:14:26 +0800

    因 CDN 域名下线，将 Bangumi API 切换回官方

--- src/common/config.js
+++ src/common/config.js
@@ -2,6 +2,6 @@ export default {
     api: {
         lavaAnime: "https://anime-api.5t5.top",
         // lavaAnime: "http://192.168.0.113:8090",
-        bangumi: "https://bgm-api.5t5.top"
+        bangumi: "https://api.bgm.tv"
     }
 }
