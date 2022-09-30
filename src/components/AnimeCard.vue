47764f11c9e3e99097949408f2dc27ebf5f1bef1
岩浆块Magma <magmablock@qq.com>
2022-09-30 12:36:46 +0800

    优化代码格式

--- src/components/AnimeCard.vue
+++ src/components/AnimeCard.vue
@@ -1,5 +1,4 @@
 <script>
-import { ref } from 'vue'
 import { switchToMyCollections, isCollected } from './Methods/MyCollections.js'
 
 export default {
@@ -39,45 +38,33 @@ export default {
 
 <template>
   <!-- 卡片 -->
-  <div
-    class="relative box-content overflow-hidden rounded-md border-2 border-white/0 transition-all ease-out hover:scale-[1.03] hover:border-2 hover:border-blue-600 hover:shadow-lg"
-  >
+  <div class="relative box-content overflow-hidden
+    rounded-md border-2 border-white/0
+    transition-all ease-out hover:scale-[1.03] hover:border-2 hover:border-blue-600 hover:shadow-lg">
     <div class="absolute h-full w-full bg-gray-100"></div>
 
     <!-- 上半：海报 + 标题 -->
-    <div
-      class="relative cursor-pointer overflow-hidden"
-      @click="goToThisAnime(id)"
-    >
+    <div class="relative cursor-pointer overflow-hidden" @click="goToThisAnime(id)">
       <!-- 图片容器 -->
       <div class="aspect-w-2 aspect-h-3 overflow-hidden">
-        <img
-          v-lazy="{
-            src: poster,
-            loading: 'https://bangumi-app-img.5t5.top/assets/PosterLoading.jpg',
-            error: 'https://bangumi-app-img.5t5.top/assets/noposter.png',
-          }"
-          class="absolute object-cover"
-          alt="封面图片"
-        />
+        <img v-lazy="{
+          src: poster,
+          loading: 'https://bangumi-app-img.5t5.top/assets/PosterLoading.jpg',
+          error: 'https://bangumi-app-img.5t5.top/assets/noposter.png',
+        }" class="absolute object-cover" alt="封面图片" />
       </div>
       <!-- 标题 -->
-      <div
-        class="absolute inset-x-0 bottom-0 grid h-24 items-end break-all bg-gradient-to-b from-transparent to-black/75 px-3 py-3 text-[13px] text-white"
-      >
+      <div class="absolute inset-x-0 bottom-0 grid h-24 items-end break-all
+        bg-gradient-to-b from-transparent to-black/75 px-3 py-3 text-[13px] text-white">
         <n-ellipsis :line-clamp="2" expand-trigger="hover">
           {{ title || '...' }}
           <!-- Special Tags -->
-          <div
-            v-if="bdrip"
-            class="ml-0.5 inline-block rounded-sm bg-blue-400 bg-opacity-50 px-1.5 text-xs font-medium backdrop-blur-sm"
-          >
+          <div v-if="bdrip" class="ml-0.5 inline-block rounded-sm px-1.5 text-xs font-medium
+            bg-blue-400 bg-opacity-50 backdrop-blur-sm">
             BD
           </div>
-          <div
-            v-if="nsfw"
-            class="ml-0.5 inline-block rounded-sm bg-yellow-300 bg-opacity-50 px-1.5 text-xs font-medium backdrop-blur-sm"
-          >
+          <div v-if="nsfw" class="ml-0.5 inline-block rounded-sm px-1.5 text-xs font-medium
+           bg-yellow-300 bg-opacity-50 backdrop-blur-sm">
             NSFW
           </div>
         </n-ellipsis>
@@ -97,19 +84,14 @@ export default {
           <!-- 菜单 -->
           <n-popover trigger="click" :show-arrow="false" raw>
             <template #trigger>
-              <div
-                class="cursor-pointer rounded p-1 hover:bg-black/20"
-                @click="updateCollected()"
-              >
+              <div class="cursor-pointer rounded p-1 hover:bg-black/20" @click="updateCollected()">
                 <i class="bi bi-list text-gray-700"></i>
               </div>
             </template>
             <div class="grid grid-cols-1 rounded bg-white p-1">
               <!-- 收藏 -->
-              <div
-                class="flex h-7 cursor-pointer select-none flex-nowrap rounded hover:bg-black/20"
-                @click="switchToMyCollections(id)"
-              >
+              <div class="flex h-7 cursor-pointer select-none flex-nowrap rounded hover:bg-black/20"
+                @click="switchToMyCollections(id)">
                 <!-- 已经收藏 -->
                 <template v-if="collected">
                   <div class="grid w-8 place-items-center text-blue-600">
@@ -128,12 +110,8 @@ export default {
                 </template>
               </div>
               <!-- 番组计划 -->
-              <a
-                v-if="parseInt(bgmid)"
-                :href="'https://bgm.tv/subject/' + bgmid"
-                target="_blank"
-                class="flex h-7 select-none flex-nowrap rounded hover:bg-black/20"
-              >
+              <a v-if="parseInt(bgmid)" :href="'https://bgm.tv/subject/' + bgmid" target="_blank"
+                class="flex h-7 select-none flex-nowrap rounded hover:bg-black/20">
                 <div class="grid w-8 place-items-center">
                   <i class="bi bi-box-arrow-up-right"></i>
                 </div>
