09ceb68d83cdb65bc9a15ce6e3b97c33501fca52
岩浆块Magma <magmablock@qq.com>
2022-10-13 11:19:37 +0800

    切换界面支持动画

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,16 @@ export default {
     <!-- 导航栏 -->
     <NavBar />
     <!-- 当前路由的界面 -->
-    <RouterView :memory="this.memory" :key="$route.path" class="overflow-y-auto w-full" />
+    <div class="overflow-y-auto w-full">
+      <RouterView v-slot="{ Component }">
+        <Transition name="fade" mode="out-in">
+          <Component :is="Component" :memory="this.memory" :key="$route.path"
+            class="overflow-y-auto w-full min-h-screen"></Component>
+        </Transition>
+      </RouterView>
+    </div>
   </div>
 </template>
 <!-- 从 /page/1 => /page/2, 由于这两个路由的 $route.path 并不一样, 所以组件被强制不复用。 -->
 <!-- 从 /page?id=1 => /page?id=2, 由于这两个路由的 $route.path 一样, 所以和没设置key属性一样, 会复用组件。 -->
 <!-- https://www.cnblogs.com/alyssa-1997/p/12187379.html -->
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router' // src/router/index.js
 import VueLazyLoad from 'vue3-lazyload'
 import './index.css' // tailwind directives
+import './transition.css' // tailwind directives
 
 const app = createApp(App)
   .use(router)
