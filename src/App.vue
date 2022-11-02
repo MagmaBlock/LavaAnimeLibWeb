b8d0fccd31a1f25ffb4522cdd8fb630ff924da37
岩浆块Magma <magmablock@qq.com>
2022-11-02 20:32:05 +0800

    更新暂停服务公告

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ export default {
           type: "10月秋"
         }
       },
+      hideNotice: false
     };
   },
   components: { RouterView, NavBar }
@@ -19,6 +20,16 @@ export default {
 
 <template>
   <ThemeProvier>
+    <div class="bg-gray-600 text-white text-center py-4 px-8" v-if="!hideNotice">
+      <div class="text-lg">【番剧库暂停播放相关服务公告】</div>
+      <p class="mb-2">
+        受微软中国区云运营代理（世纪互联）最新的策略调整影响，我们目前已不能继续在中国大陆为各位提供视频播放服务。<br>
+        番剧库在建站初期已经预料到这一情况的发生，在接下来的数日内，番剧库将暂停播放相关服务，直至新的文件分发方式被找到。<br>
+        番剧库的番剧资源（约2.5TB）仍有完整的备份，部署需要一定的时间（大约数日），<br>
+        另外，可能会有一些资源管理相关的代码需要重写，刚好加上最近比较忙，请耐心等待。
+      </p>
+      <n-button @click="hideNotice = true">关闭</n-button>
+    </div>
     <div class="flex flex-row flex-nowrap h-screen max-w-[2560px] mx-auto shadow-lg
     dark:bg-neutral-900 text-gray-800 dark:text-zinc-200">
       <!-- 导航栏 -->
