90d91c555af784fada12c7f1615de692cce1bf6e
岩浆块Magma <magmablock@qq.com>
2022-12-05 17:28:26 +0800

    优化轮播图性能

--- src/components/Home/HeaderPictures.vue
+++ src/components/Home/HeaderPictures.vue
@@ -3,7 +3,11 @@
     <template v-for="pic in headerPic">
       <div class="static w-full h-52 sm:h-60">
         <!-- 真图片 -->
-        <img class="object-cover w-full min-h-full overflow-hidden" :src="pic.pic">
+        <img class="object-cover w-full min-h-full overflow-hidden" v-lazy="{
+          src: pic.pic,
+          loading: '/Home/headerPic/LavaAnime.jpg',
+          error: '/Home/headerPic/LavaAnime.jpg'
+        }">
         <!-- 底部阴影 -->
         <div class="absolute inset-x-0 bottom-0 h-32 bg-gradient-to-b from-transparent to-black/75 "></div>
         <RouterLink v-if="pic.url" :to="pic.url">
