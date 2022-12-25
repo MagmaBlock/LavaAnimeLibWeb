9920fe123b4ce5b439ae77f5f0365cd3b3de881c
岩浆块Magma <magmablock@qq.com>
2022-12-25 23:56:33 +0800

    修复 Animate.css 引入优先级过高造成的样式污染

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // src/router/index.js
 import VueLazyLoad from 'vue3-lazyload'
+import 'animate.css';
 import './index.css' // tailwind directives
 import './transition.css' // 自定义的切换动画
-import 'animate.css';
 
 // 在 Tailwind 后插入 NaiveUI 的 css
 const meta = document.createElement('meta')
