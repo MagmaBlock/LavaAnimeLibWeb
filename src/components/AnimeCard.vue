f807d58aa188a01ef19ef4633156ce3d2309852b
岩浆块Magma <magmablock@qq.com>
2022-12-26 00:11:56 +0800

    番剧卡点击改为Link

--- src/components/AnimeCard.vue
+++ src/components/AnimeCard.vue
@@ -43,31 +43,33 @@ export default {
     <div class="absolute h-full w-full bg-gray-100 dark:bg-gray-500"></div>
 
     <!-- 上半：海报 + 标题 -->
-    <div class="relative cursor-pointer overflow-hidden" @click="goToThisAnime(id)">
-      <!-- 图片容器 -->
-      <div class="aspect-w-2 aspect-h-3 overflow-hidden">
-        <img v-lazy="{
-          src: poster,
-          loading: 'https://bangumi-app-img.5t5.top/assets/PosterLoading.jpg',
-          error: 'https://bangumi-app-img.5t5.top/assets/noposter.png',
-        }" class="absolute object-cover" alt="封面图片" />
-      </div>
-      <!-- 标题 -->
-      <div class="absolute inset-x-0 bottom-0 grid h-24 items-end break-all
+    <div class="relative cursor-pointer overflow-hidden">
+      <RouterLink :to="{ name: 'Anime', params: { la: id } }">
+        <!-- 图片容器 -->
+        <div class="aspect-w-2 aspect-h-3 overflow-hidden">
+          <img v-lazy="{
+            src: poster,
+            loading: 'https://bangumi-app-img.5t5.top/assets/PosterLoading.jpg',
+            error: 'https://bangumi-app-img.5t5.top/assets/noposter.png',
+          }" class="absolute object-cover" alt="封面图片" />
+        </div>
+        <!-- 标题 -->
+        <div class="absolute inset-x-0 bottom-0 grid h-24 items-end break-all
         bg-gradient-to-b from-transparent to-black/75 px-3 py-3 text-[13px] text-white">
-        <n-ellipsis :line-clamp="2" expand-trigger="hover">
-          {{ title || '...' }}
-          <!-- Special Tags -->
-          <div v-if="bdrip" class="ml-0.5 inline-block rounded-sm px-1.5 text-xs font-medium
+          <n-ellipsis :line-clamp="2" expand-trigger="hover">
+            {{ title || '...' }}
+            <!-- Special Tags -->
+            <div v-if="bdrip" class="ml-0.5 inline-block rounded-sm px-1.5 text-xs font-medium
             bg-blue-400 bg-opacity-50 backdrop-blur-sm">
-            BD
-          </div>
-          <div v-if="nsfw" class="ml-0.5 inline-block rounded-sm px-1.5 text-xs font-medium
+              BD
+            </div>
+            <div v-if="nsfw" class="ml-0.5 inline-block rounded-sm px-1.5 text-xs font-medium
            bg-yellow-300 bg-opacity-50 backdrop-blur-sm">
-            NSFW
-          </div>
-        </n-ellipsis>
-      </div>
+              NSFW
+            </div>
+          </n-ellipsis>
+        </div>
+      </RouterLink>
     </div>
 
     <!-- 信息区 -->
