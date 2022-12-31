ba28eb188601e10c562fcc4c6d4a8e0b17a22feb
岩浆块Magma <magmablock@qq.com>
2022-12-31 15:43:31 +0800

    创建可复用的响应式全局 userInfo 对象

---
+++ src/common/API/user.js
@@ -0,0 +1,25 @@
+import { ref } from "vue";
+import { LavaAnimeAPI } from "../api";
+
+// 响应式的全局用户信息对象. 任何组件调用下方的刷新函数, 此对象都会被响应式更新一次
+export const userInfo = ref({})
+
+/**
+ * 刷新用户信息
+ * @returns {Object} userInfo
+ */
+export async function getUserInfo() {
+  try {
+    let result = await LavaAnimeAPI.get('/v2/user/info')
+    if (result.data.code == 200) {
+      userInfo.value = result.data.data
+      return userInfo
+    }
+  } catch (error) {
+    if (error.status == 401) {
+      $message.error('未登录')
+      userInfo.value = {}
+    }
+    return userInfo
+  }
+}
