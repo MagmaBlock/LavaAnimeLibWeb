fffac37a623c4b658e0fb8032ffc2b277e07d9da
岩浆块Magma <magmablock@qq.com>
2023-01-01 16:41:01 +0800

    优化搜索页早期留下的不成熟代码；搜索记录使用 VueUse 库支持响应式

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -4,13 +4,18 @@ import Container from '../components/Container.vue';
 import AnimeCardContainer from '../components/Container/AnimeCardContainer.vue';
 import SearchBar from '../components/Search/SearchBar.vue';
 
+import { useStorage } from '@vueuse/core'
+
 export default {
+  setup() {
+    const searchHistory = useStorage('searchHistory', [])
+    return { searchHistory }
+  },
   data() {
     return {
       searchValue: '',
       searchTimes: 0,
       searchResults: [],
-      searchHistory: [],
       searchRecommendation: [],
     };
   },
@@ -36,24 +41,9 @@ export default {
         console.error('搜索发生错误: ', error);
       }
     },
-    loadSearchHistory() {
-      let localStorageHistory = JSON.parse(localStorage.getItem("searchHistory"));
-      if (typeof localStorageHistory == "object" && localStorageHistory) {
-        console.log("searchHistory from localStorage: ", localStorageHistory);
-        this.searchHistory = localStorageHistory;
-      }
-    },
     addSearchHistory(value) {
-      let newList = [value];
-      for (let i in this.searchHistory) {
-        if (this.searchHistory[i] != value && i <= 10) {
-          newList.push(this.searchHistory[i]);
-        }
-      }
-      this.searchHistory = newList;
-    },
-    clearSearchHistroy() {
-      this.searchHistory = [];
+      this.searchHistory.unshift(value)
+      this.searchHistory = Array.from(new Set(this.searchHistory))
     },
     // 尝试使用 URL 参数或者 sessionStorage 还原搜索词
     useUrlParams() {
@@ -77,7 +67,6 @@ export default {
           this.searchRecommendation = result.data.data
         }
       } catch (error) { }
-
     }
   },
   computed: {
@@ -88,20 +77,14 @@ export default {
         'mr-2', 'mb-2', 'px-2', 'py-[3px]', 'rounded', 'max-w-xs', 'overflow-hidden', 'grid', 'place-items-center']
     },
     clearTagClass() {
-      return `bg-blue-50 hover:bg-blue-200 dark:bg-blue-900/30 dark:hover:bg-blue-900/75
+      return `bg-blue-50 hover:bg-blue-200 dark:bg-blue-900/30 dark:hover:bg-blue-900/75 text-blue-500
       ease-in duration-200 cursor-pointer mr-2 mb-2 px-2 grid place-items-center rounded max-w-xs text-ellipsis overflow-hidden`
     }
   },
   mounted() {
     document.title = '搜索 | 熔岩番剧库 LavaAnimeLib'
-    this.loadSearchHistory();
     this.useUrlParams();
     this.getHotAnimes()
-  },
-  watch: {
-    searchHistory() {
-      localStorage.setItem("searchHistory", JSON.stringify(this.searchHistory)); // JSON 存储至 localHistory
-    }
   },
   components: { Container, SearchBar, AnimeCardContainer }
 }
@@ -109,52 +92,46 @@ export default {
 
 <template>
   <Container>
-    <div class="lg:flex">
-      <!-- 搜索框容器部分，在高宽度屏幕上将左右 Flex -->
-      <div class="lg:basis-1/4 flex-none">
-        <!-- 搜索本体部分，将粘连屏幕 -->
-        <div class="sticky top-5 select-none">
-          <div class="text-lg mb-4 mx-0.5 font-medium">搜索</div>
-          <!-- 搜索框 -->
-          <SearchBar v-model:search="searchValue" @search="value => search(value)" />
-          <!-- 历史记录 -->
-          <div class="my-4 w-full flex flex-wrap">
-            <!-- 标签 -->
-            <span v-for="value in searchHistory" @click="search(value)" :class="normalTagClass">
-              {{ value }}
-            </span>
-            <!-- 清除按钮 -->
-            <n-tooltip trigger="hover">
-              <template #trigger>
-                <span :class="clearTagClass" @click="clearSearchHistroy()" v-if="this.searchHistory.length > 0">
-                  <div class="text-blue-500">
-                    <i class="bi bi-x-lg"></i>
-                  </div>
-                </span>
-                <span v-else></span>
-              </template>
-              删除历史记录
-            </n-tooltip>
-          </div>
-          <!-- 搜索推荐 -->
-          <Transition name="fade">
-            <div v-if="searchRecommendation.length">
-              <div class="text-lg mb-4 mx-0.5 font-medium">大家在看</div>
-              <div class="my-4 w-full flex flex-wrap">
-                <span v-for="value in searchRecommendation" :class="normalTagClass">
-                  <RouterLink :to="{ name: 'Anime', params: { la: value.id } }">
-                    {{ value.title }}
-                  </RouterLink>
-                </span>
-              </div>
-            </div>
-          </Transition>
+    <div class="lg:grid lg:grid-cols-4 gap-4">
+      <!-- 搜索本体部分，将粘连屏幕 -->
+      <!-- 由于 Grid 布局默认将所有子元素拉伸至总高，因此我们在这里使用 self-start 来让元素向上对齐 -->
+      <div class="lg:col-span-1 lg:sticky lg:top-8 lg:self-start mb-4 select-none">
+        <div class="text-lg mb-4 mx-0.5 font-medium">搜索</div>
+        <!-- 搜索框 -->
+        <SearchBar v-model:search="searchValue" @search="value => search(value)" />
+        <!-- 历史记录 -->
+        <div class="mt-4 w-full flex flex-wrap" v-if="this.searchHistory.length">
+          <!-- 标签 -->
+          <span v-for="value in searchHistory" @click="search(value)" :class="normalTagClass">
+            {{ value }}
+          </span>
+          <!-- 清除按钮 -->
+          <n-tooltip trigger="hover">
+            <template #trigger>
+              <span :class="clearTagClass" @click="searchHistory = []">
+                <i class="bi bi-x-lg"></i>
+              </span>
+            </template>
+            删除历史记录
+          </n-tooltip>
         </div>
+        <!-- 搜索推荐 -->
+        <Transition name="fade">
+          <div v-if="searchRecommendation.length">
+            <div class="text-lg mt-4 mx-0.5 font-medium">大家在看</div>
+            <div class="mt-4 w-full flex flex-wrap">
+              <span v-for="value in searchRecommendation" :class="normalTagClass">
+                <RouterLink :to="{ name: 'Anime', params: { la: value.id } }">
+                  {{ value.title }}
+                </RouterLink>
+              </span>
+            </div>
+          </div>
+        </Transition>
       </div>
 
       <!-- 内容部分 -->
-      <AnimeCardContainer :animes="searchResults" size="large" class="lg:basis-3/4 flex-none px-2 lg:px-4 "
-        v-if="searchTimes" />
+      <AnimeCardContainer :animes="searchResults" size="large" class="lg:col-span-3" v-if="searchTimes" />
     </div>
   </Container>
 </template>
