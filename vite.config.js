0bf68b6e882b01a3b2890fc35ceb5ea37a9eb31b
岩浆块Magma <magmablock@qq.com>
2023-01-21 02:17:00 +0800

    开发环境不再使用PWA；升级vueuse

--- vite.config.js
+++ vite.config.js
@@ -42,7 +42,7 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
       devOptions: {
-        enabled: true,
+        enabled: false,
       },
     }),
   ],
