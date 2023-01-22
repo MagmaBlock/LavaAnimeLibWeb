40c1ff50dfc30af1ada1d658e8fc6f197ee7c6d3
岩浆块Magma <magmablock@qq.com>
2023-01-22 09:52:24 +0800

    优化 显示更多按钮视口事件也可通过点击触发

--- src/components/Container/ShowMoreButton.vue
+++ src/components/Container/ShowMoreButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid place-items-center mt-8" ref="ShowMoreButton">
+  <div class="grid place-items-center mt-8" ref="ShowMoreButton" @click="onSee">
     <div class="text-gray-500 dark:text-zinc-300 bg-gray-100 dark:bg-zinc-800 
         hover:bg-gray-200 hover:dark:bg-zinc-700 active:bg-gray-300 active:dark:bg-zinc-600
         ease-in duration-200 rounded-md px-12 py-1">显示更多</div>
