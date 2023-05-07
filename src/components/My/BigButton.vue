cde029e7345e0c27c0d3d4c6f2a5f701fd420cfe
岩浆块Magma <magmablock@qq.com>
2023-05-07 15:14:51 +0800

    优化 调整个人菜单的功能按钮图标；

--- src/components/My/BigButton.vue
+++ src/components/My/BigButton.vue
@@ -2,27 +2,25 @@
   <span>
     <!-- 宽屏 -->
     <div
-      class="hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md hidden sm:grid grid-cols-1"
+      class="hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md hidden sm:grid grid-cols-1 py-4"
     >
-      <div class="py-4">
-        <div
-          class="text-gray-700 dark:text-zinc-100 w-full text-3xl mb-1 text-center"
-        >
+      <div
+        class="grid place-items-center text-gray-700 dark:text-zinc-100 text-xs w-full text-center"
+      >
+        <n-icon :size="40">
           <slot name="icon"></slot>
-        </div>
-        <div
-          class="text-gray-700 dark:text-zinc-100 w-full text-xs text-center"
-        >
-          <slot name="name"></slot>
-        </div>
+        </n-icon>
+        <slot name="name"></slot>
       </div>
     </div>
     <!-- 手机 -->
     <div class="sm:hidden">
-      <div class="text-gray-700 dark:text-zinc-100 w-full text-2xl text-center">
-        <slot name="icon"></slot>
-      </div>
-      <div class="text-gray-700 dark:text-zinc-100 w-full text-xs text-center">
+      <div
+        class="grid place-items-center text-gray-700 dark:text-zinc-100 text-xs w-full text-center"
+      >
+        <n-icon :size="30">
+          <slot name="icon"></slot>
+        </n-icon>
         <slot name="name"></slot>
       </div>
     </div>
--- src/components/My/FunctionsCard.vue
+++ src/components/My/FunctionsCard.vue
@@ -6,28 +6,28 @@
       <!-- 个人设置 -->
       <BigButton @click="$router.push({ name: 'UserInfo' })">
         <template #icon>
-          <i class="bi bi-person-lines-fill"></i>
+          <AccountBoxFilled />
         </template>
         <template #name>个人信息</template>
       </BigButton>
       <!-- 帮助 -->
       <BigButton @click="$router.push('/help')">
         <template #icon>
-          <i class="bi bi-book-half"></i>
+          <ArticleOutlined />
         </template>
         <template #name>使用帮助</template>
       </BigButton>
       <!-- 深色模式 -->
       <BigButton @click="$refs.darkModeModel.show = true">
         <template #icon>
-          <i class="bi bi-moon-fill"></i>
+          <NightsStayRound />
         </template>
         <template #name>深色模式</template>
       </BigButton>
       <DarkModeModel ref="darkModeModel"></DarkModeModel>
       <BigButton @click="$router.push({ name: 'Update' })">
         <template #icon>
-          <i class="bi bi-bar-chart-steps"></i>
+          <UpdateFilled />
         </template>
         <template #name>更新日志</template>
       </BigButton>
@@ -38,12 +38,25 @@
 <script>
 import MyBasicCard from "./MyBasicCard.vue";
 import BigButton from "./BigButton.vue";
+import {
+  AccountBoxFilled,
+  ArticleOutlined,
+  NightsStayRound,
+  UpdateFilled,
+} from "@vicons/material";
 
 export default {
   data() {
     return {};
   },
   methods: {},
-  components: { BigButton, MyBasicCard },
+  components: {
+    BigButton,
+    MyBasicCard,
+    AccountBoxFilled,
+    ArticleOutlined,
+    NightsStayRound,
+    UpdateFilled,
+  },
 };
 </script>
