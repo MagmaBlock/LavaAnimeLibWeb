4904bbe5ca8af113b46e2bac8fb66b16b4a35097
岩浆块Magma <magmablock@qq.com>
2023-05-14 21:31:41 +0800

    优化 默认配置增加注释

--- src/common/config.js
+++ src/common/config.js
@@ -1,7 +1,10 @@
 export default {
-    api: {
-        lavaAnime: "https://anime-api.5t5.top",
-        // lavaAnime: "http://192.168.0.113:8090",
-        bangumi: "https://api.bgm.tv"
-    }
-}
+  api: {
+    // 定义后端的 Host
+    lavaAnime: "https://anime-api.5t5.top", // 默认线上后端
+    // lavaAnime: "http://127.0.0.1:8090", // 本地开发
+
+    // 定义 Bangumi API 的 Host
+    bangumi: "https://api.bgm.tv",
+  },
+};
