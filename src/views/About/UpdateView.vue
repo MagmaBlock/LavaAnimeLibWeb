f241da45fd567fc7c01cae0ea3809b7c94f3ad39
岩浆块Magma <magmablock@qq.com>
2023-06-07 23:05:10 +0800

    优化 更新历史页顶栏

--- src/views/About/UpdateView.vue
+++ src/views/About/UpdateView.vue
@@ -1,51 +1,46 @@
 <template>
-  <div>
+  <Container>
     <TopNav title="更新日志"></TopNav>
-    <Container>
-      <div class="text-lg font-medium">熔岩番剧库 最近更新日志</div>
-      <div class="opacity-75">LavaAnimeLib 相关项目 GitHub 提交记录</div>
-      <n-divider />
-      <!-- 宽屏 -->
-      <div class="hidden lg:grid grid-cols-2 gap-4">
-        <div>
-          <n-h5 class="text-center">网页（前端）</n-h5>
+    <div class="text-lg font-medium mt-4">熔岩番剧库 最近更新日志</div>
+    <div class="opacity-75">LavaAnimeLib 相关项目 GitHub 提交记录</div>
+    <n-divider />
+    <!-- 宽屏 -->
+    <div class="hidden lg:grid grid-cols-2 gap-4">
+      <div>
+        <n-h5 class="text-center">网页（前端）</n-h5>
+        <GitHubCommits :commits="web"></GitHubCommits>
+      </div>
+      <div>
+        <n-h5 class="text-center">服务端（后端）</n-h5>
+        <GitHubCommits :commits="server"></GitHubCommits>
+      </div>
+    </div>
+    <!-- 移动 -->
+    <div class="lg:hidden">
+      <n-tabs type="line" animated justify-content="space-evenly">
+        <n-tab-pane name="web" tab="网页前端">
           <GitHubCommits :commits="web"></GitHubCommits>
-        </div>
-        <div>
-          <n-h5 class="text-center">服务端（后端）</n-h5>
+        </n-tab-pane>
+        <n-tab-pane name="server" tab="服务后端">
           <GitHubCommits :commits="server"></GitHubCommits>
-        </div>
+        </n-tab-pane>
+      </n-tabs>
+    </div>
+    <n-divider>
+      <div class="flex gap-2">
+        更多记录见 GitHub
+        <n-a
+          target="_blank"
+          href="https://github.com/MagmaBlock/LavaAnimeLibWeb"
+        >
+          前端
+        </n-a>
+        <n-a target="_blank" href="https://github.com/MagmaBlock/LavaAnimeLib">
+          后端
+        </n-a>
       </div>
-      <!-- 移动 -->
-      <div class="lg:hidden">
-        <n-tabs type="line" animated justify-content="space-evenly">
-          <n-tab-pane name="web" tab="网页前端">
-            <GitHubCommits :commits="web"></GitHubCommits>
-          </n-tab-pane>
-          <n-tab-pane name="server" tab="服务后端">
-            <GitHubCommits :commits="server"></GitHubCommits>
-          </n-tab-pane>
-        </n-tabs>
-      </div>
-      <n-divider>
-        <div class="flex gap-2">
-          更多记录见 GitHub
-          <n-a
-            target="_blank"
-            href="https://github.com/MagmaBlock/LavaAnimeLibWeb"
-          >
-            前端
-          </n-a>
-          <n-a
-            target="_blank"
-            href="https://github.com/MagmaBlock/LavaAnimeLib"
-          >
-            后端
-          </n-a>
-        </div>
-      </n-divider>
-    </Container>
-  </div>
+    </n-divider>
+  </Container>
 </template>
 <script>
 import axios from "axios";
