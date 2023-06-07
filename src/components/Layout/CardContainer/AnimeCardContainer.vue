adbc7eb0e21754432e1ce4dd8c76fa7e6e126034
岩浆块Magma <magmablock@qq.com>
2023-06-07 21:51:50 +0800

    修复 番剧卡片容器在首页持续占用 CPU 的性能问题；优化 使用 Setup 重构番剧卡片容器组件；

--- src/components/Layout/CardContainer/AnimeCardContainer.vue
+++ src/components/Layout/CardContainer/AnimeCardContainer.vue
@@ -1,98 +1,61 @@
-<script>
+<script setup>
+import { computed, onMounted, ref, watch } from "vue";
+import { useElementVisibility } from "@vueuse/core";
 import AnimeCard from "../../AnimeCard/AnimeCard.vue";
 import ShowMoreButton from "./ShowMoreButton.vue";
 
-export default {
-  data() {
-    return {
-      page: null,
-    };
-  },
-  props: {
-    animes: Array, // 请传入一个包含动画信息的数组，数组中的每个对象均有 AnimeCard 所需的参数
-    size: String, // 决定屏幕宽度  目前有 full large half alwaysMobile
-    fakeNumber: { type: Number, default: 18 }, // 未加载情况下显示多少个骨架卡片
-    loading: Boolean, // 决定是否显示为加载状态
-  },
-  watch: {
-    animes(newList, oldList) {
-      this.page = 29; // 30 个
-    },
-  },
-  mounted() {
-    this.page = 29;
-  },
-  computed: {
-    autoGroupClass() {
-      switch (this.size) {
-        case "full":
-          return this.fullClass;
-        case "large":
-          return this.largeClass;
-        case "half":
-          return this.halfClass;
-        case "alwaysMobile":
-          return this.alwaysMobileClass;
-        default:
-          return "";
-      }
-    },
-    fullClass() {
-      return `grid grid-cols-3 gap-2
-        sm:grid-cols-4 sm:gap-x-4
-        md:grid-cols-5 md:gap-x-6
-        lg:grid-cols-7
-        xl:grid-cols-8
-        2xl:grid-cols-10`;
-    },
-    largeClass() {
-      return `grid grid-cols-3 gap-2
-        sm:grid-cols-4 sm:gap-x-4
-        md:grid-cols-5 md:gap-x-6
-        lg:grid-cols-4
-        xl:grid-cols-5
-        2xl:grid-cols-6`;
-    },
-    halfClass() {
-      return `grid grid-cols-3 gap-2
-        sm:grid-cols-4
-        md:grid-cols-5
-        lg:grid-cols-3 lg:gap-4
-        xl:grid-cols-4
-        2xl:grid-cols-5
-      `;
-    },
-    alwaysMobileClass() {
-      return `grid grid-cols-3 gap-3
-        sm:grid-cols-3
-      `;
-    },
-  },
-  components: { ShowMoreButton, AnimeCard },
-};
+const props = defineProps({
+  animes: Array, // 请传入一个包含动画信息的数组，数组中的每个对象均有 AnimeCard 所需的参数
+  size: String, // 决定屏幕宽度  目前有 full large half alwaysMobile
+  fakeNumber: { type: Number, default: 18 }, // 未加载情况下显示多少个骨架卡片
+  loading: Boolean, // 决定是否显示为加载状态
+});
+
+// 展示的卡片数量
+const page = ref(29);
+// 每次动画列表更换后, 重置展示数量
+watch(
+  () => props.animes,
+  () => {
+    page.value = 29;
+  }
+);
+
+const autoGroupClass = computed(() => {
+  return {
+    full: "grid grid-cols-3 gap-2 sm:grid-cols-4 sm:gap-x-4 md:grid-cols-5 md:gap-x-6 lg:grid-cols-7 xl:grid-cols-8 2xl:grid-cols-10",
+    large:
+      "grid grid-cols-3 gap-2 sm:grid-cols-4 sm:gap-x-4 md:grid-cols-5 md:gap-x-6 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6",
+    half: "grid grid-cols-3 gap-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-3 lg:gap-4 xl:grid-cols-4 2xl:grid-cols-5",
+    alwaysMobile: "grid grid-cols-3 gap-3 sm:grid-cols-3",
+  }[props.size];
+});
+
+const target = ref(null);
+const visible = useElementVisibility(target);
 </script>
 
 <template>
-  <div class="select-none w-full">
-    <n-spin :show="!animes || loading" class="w-full">
+  <div class="select-none w-full" ref="target">
+    <n-spin :show="visible && (loading || !animes)" class="w-full">
       <div :class="autoGroupClass" class="w-full">
         <!-- 骨架屏 -->
         <template v-if="!animes" v-for="a in fakeNumber">
-          <AnimeCard></AnimeCard>
+          <AnimeCard />
         </template>
         <!-- 番剧卡片 -->
         <template v-for="(anime, index) in animes">
           <AnimeCard class="self-start" :anime="anime" v-if="index <= page" />
         </template>
       </div>
-      <ShowMoreButton v-if="animes?.length > page" @click="page = page + 30" />
-      <n-empty
-        size="large"
-        description="太可惜了，什么也没找到"
-        class="py-16"
-        v-if="animes?.length == 0"
-      >
-      </n-empty>
     </n-spin>
+    <ShowMoreButton v-if="animes?.length > page" @click="page = page + 30" />
+    <n-empty
+      size="large"
+      description="太可惜了，什么也没找到"
+      class="py-16"
+      v-if="animes?.length == 0"
+    >
+    </n-empty>
   </div>
 </template>
