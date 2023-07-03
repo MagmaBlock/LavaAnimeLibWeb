88d6003f986252ca94d9ce877ee8ee40ff6fa5cc
岩浆块Magma <magmablock@qq.com>
2023-07-04 00:17:48 +0800

    新增 左菜单右容器组件支持自定义页尾区域

--- src/components/Layout/PageLayout/LeftMenuRightContent.vue
+++ src/components/Layout/PageLayout/LeftMenuRightContent.vue
@@ -6,9 +6,17 @@
     <!-- 由于 Grid 布局默认将所有子元素拉伸至总高，因此我们在这里使用 self-start 来让元素向上对齐 -->
     <div class="lg:col-span-1 xl:col-span-2 lg:self-start lg:sticky lg:top-8">
       <slot name="left"></slot>
+
+      <div class="hidden lg:block">
+        <slot name="foot"></slot>
+      </div>
     </div>
     <div class="lg:col-span-2 xl:col-span-5">
       <slot name="right"></slot>
+      
+      <div class="lg:hidden">
+        <slot name="foot"></slot>
+      </div>
     </div>
   </div>
 </template>
