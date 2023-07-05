70fc9fec74229b1d2545c1ce839538585a5a111c
岩浆块Magma <magmablock@qq.com>
2023-07-06 02:57:36 +0800

    新增 番剧库运营工具

--- src/components/Anime/AnimeDataCard.vue
+++ src/components/Anime/AnimeDataCard.vue
@@ -8,10 +8,32 @@ const store = useAnimeStore();
 
 const showMore = ref(false);
 
+// 运营工具
+const showTools = ref(false);
+const { copy } = useClipboard();
 const getPath = computed(() => {
   return `D:\\OneDrive - MSFT\\LavaAnimeLib\\${store.animeData?.index.year}\\${store.animeData?.index.type}\\${store.animeData?.index.name}`;
 });
-const { copy, copied } = useClipboard();
+const getRuleName = computed(() => {
+  let month = store.animeData?.index.type.match(/^\d{1,2}/);
+  if (!month) {
+    month = "other";
+  }
+  return `【${month}】${store.animeData?.index.name}`;
+});
+const getWebsite = computed(() => {
+  if (!store.animeData?.infobox) return;
+  let result = store.animeData?.infobox?.find((kv) => {
+    return ["官方网站", "官网", "网站"].includes(kv.key);
+  });
+  if (result?.value) {
+    return [
+      result.value,
+      result.value + (result.value.endsWith("/") ? "story" : "/story"),
+      result.value + (result.value.endsWith("/") ? "episodes" : "/episodes"),
+    ];
+  } else return;
+});
 </script>
 
 <template>
@@ -24,16 +46,56 @@ const { copy, copied } = useClipboard();
         {{ store.animeData?.index.type }}
         <i class="bi bi-chevron-right text-xs"></i>
       </RouterLink>
-      <!-- 临时小工具 -->
-      <n-tooltip placement="bottom" trigger="click">
-        <template #trigger>
-          {{ store.animeData?.index.name }}
-        </template>
-        {{ getPath }}
-        <NButton @click="copy(getPath)" size="small">
-          {{ copied ? "已复制" : "复制" }}
-        </NButton>
-      </n-tooltip>
+      <!-- (临时) 小工具 -->
+      <span class="ml-1 cursor-help" @click="() => (showTools = true)">
+        {{ store.animeData?.index.name }}
+      </span>
+      <NDrawer
+        v-model:show="showTools"
+        placement="bottom"
+        :default-height="540"
+        resizable
+      >
+        <NDrawerContent title="番剧库运营工具">
+          <n-list bordered>
+            <n-list-item>
+              <n-thing title="RuleName" :description="getRuleName"> </n-thing>
+              <template #suffix>
+                <NButton @click="copy(getRuleName)"> 复制 </NButton>
+              </template>
+            </n-list-item>
+            <n-list-item>
+              <n-thing title="Path" :description="getPath"> </n-thing>
+              <template #suffix>
+                <NButton @click="copy(getPath)"> 复制 </NButton>
+              </template>
+            </n-list-item>
+            <n-list-item v-if="store.animeData?.name">
+              <n-thing title="Name" :description="store.animeData.name">
+              </n-thing>
+              <template #suffix>
+                <NButton @click="copy(store.animeData.name)"> 复制 </NButton>
+              </template>
+            </n-list-item>
+            <n-list-item v-if="store.animeData?.name_cn">
+              <n-thing title="NameCN" :description="store.animeData.name_cn">
+              </n-thing>
+              <template #suffix>
+                <NButton @click="copy(store.animeData.name_cn)"> 复制 </NButton>
+              </template>
+            </n-list-item>
+            <n-list-item v-if="getWebsite">
+              <n-thing title="Website"> </n-thing>
+              <template v-for="url in getWebsite">
+                <NA target="_blank" :href="url">
+                  {{ url }}
+                </NA>
+                <br />
+              </template>
+            </n-list-item>
+          </n-list>
+        </NDrawerContent>
+      </NDrawer>
     </div>
     <!-- 主信息卡 -->
     <div class="py-3 px-4">
