a35794a81cc5b0f0e0f5e2fad47a7f9812afe5f1
岩浆块Magma <magmablock@qq.com>
2023-07-18 17:46:00 +0800

    移除 旧版浏览器内的番剧收藏功能

--- src/views/User/UserView.vue
+++ src/views/User/UserView.vue
@@ -4,7 +4,6 @@
       <UserCard class="px-6 py-4 h-full" />
       <FunctionsCard class="p-2 h-full" />
     </div>
-    <MyCollections class="px-6" />
     <MyFollow class="px-6 sm:my-4" />
     <AdminLinks class="mx-6 sm:mx-0" />
   </ContainerMobileFull>
@@ -15,7 +14,6 @@ import { onMounted } from "vue";
 import ContainerMobileFull from "../../components/Layout/PageContainer/ContainerMobileFull.vue";
 import UserCard from "../../components/My/User/UserCard.vue";
 import FunctionsCard from "../../components/My/FunctionsCard.vue";
-import MyCollections from "../../components/My/MyCollections.vue";
 import MyFollow from "../../components/My/MyFollow.vue";
 import AdminLinks from "../../components/Admin/AdminLinks.vue";
 
