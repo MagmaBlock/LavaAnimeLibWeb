725b0de6fa53f1564252f4464a454b9ce1de234a
岩浆块Magma <magmablock@qq.com>
2023-07-21 16:45:16 +0800

    新增 播放记录卡片支持刷新，支持提示用户“刚刚正在观看”

--- src/components/My/ViewHistory/MyViewHistory.vue
+++ src/components/My/ViewHistory/MyViewHistory.vue
@@ -1,48 +1,96 @@
 <template>
   <n-card title="观看历史" :bordered="false" embedded>
-    <!-- TODO -->
-    <!-- <template #header-extra>
-      <n-button secondary round size="small" icon-placement="right">
-        <template #icon>
-          <n-icon> <ChevronRightFilled /> </n-icon>
-        </template>
-      </n-button>
-    </template> -->
-    <div class="relative">
+    <!-- Buttons -->
+    <template #header-extra>
+      <n-space>
+        <n-button secondary round size="small" @click="buildPage">
+          <template #icon>
+            <n-icon>
+              <RefreshFilled :class="loading ? 'animate-spin' : ''" />
+            </n-icon>
+          </template>
+        </n-button>
+        <RouterLink :to="{ name: 'UserHistory' }">
+          <n-button secondary round size="small">
+            <template #icon>
+              <n-icon> <ChevronRightFilled /> </n-icon>
+            </template>
+          </n-button>
+        </RouterLink>
+      </n-space>
+    </template>
+    <TransitionGroup name="fade" class="relative">
+      <!-- 正常数据 -->
       <div class="flex gap-2 overflow-x-scroll" ref="scroll" v-if="data.length">
-        <HistoryRecord v-for="history in data" :record="history" />
+        <template v-for="(history, index) in data">
+          <n-popover
+            trigger="manual"
+            class="animate-bounce"
+            :show="justWatched && showPop && index == 0"
+          >
+            <template #trigger>
+              <Transition name="fade" mode="out-in">
+                <HistoryRecord
+                  :record="history"
+                  :key="history.lastReportTime"
+                />
+              </Transition>
+            </template>
+            <Transition name="fade">
+              <div>✨ 刚刚正在观看</div>
+            </Transition>
+          </n-popover>
+        </template>
       </div>
       <!-- 翻页按钮 -->
-      <TransitionGroup name="fade" v-if="data.length">
-        <ScrollButton
-          v-if="!arrivedState.left"
-          class="absolute -left-4 inset-y-0 grid place-items-center"
-          direction="left"
-          @click="() => scrollAction('left')"
-        />
-        <ScrollButton
-          v-if="!arrivedState.right"
-          class="absolute -right-4 inset-y-0 grid place-items-center"
-          direction="right"
-          @click="() => scrollAction('right')"
-        />
-      </TransitionGroup>
-      <n-empty v-if="!data.length" description="什么也没有看过"></n-empty>
-    </div>
+      <ScrollButton
+        v-if="!arrivedState.left && data.length"
+        class="absolute -left-4 inset-y-0 grid place-items-center"
+        direction="left"
+        @click="() => scrollAction('left')"
+      />
+      <ScrollButton
+        v-if="!arrivedState.right && data.length"
+        class="absolute -right-4 inset-y-0 grid place-items-center"
+        direction="right"
+        @click="() => scrollAction('right')"
+      />
+      <!-- 空状态 -->
+      <n-empty v-if="!data.length && !loading" description="什么也没有看过" />
+    </TransitionGroup>
   </n-card>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { LavaAnimeAPI } from "../../../common/api";
 import HistoryRecord from "./HistoryRecord.vue";
 import { useScroll } from "@vueuse/core";
 import ScrollButton from "./ScrollButton.vue";
-import { ChevronRightFilled } from "@vicons/material";
+import { ChevronRightFilled, RefreshFilled } from "@vicons/material";
 
 const data = ref([]);
-const scroll = ref(null);
+const showPop = ref(true); // 刚打开界面 10s 内展示提示
+const loading = ref(true);
 
+buildPage();
+async function buildPage() {
+  data.value = [];
+  loading.value = true;
+  await getHistory();
+
+  setTimeout(() => {
+    loading.value = false;
+  }, 300);
+  showPop.value = true;
+
+  setTimeout(() => {
+    showPop.value = false;
+  }, 8000);
+}
+
+// 横向滚动
+const scroll = ref(null);
 const { x, arrivedState } = useScroll(scroll, { behavior: "smooth" });
 const scrollAction = (direction) => {
   if (direction == "right") {
@@ -67,5 +115,21 @@ async function getHistory(page, pageSize) {
   } catch (error) {}
 }
 
-getHistory();
+// 如果 1 分钟内有最新的历史记录, 此项为 true
+const justWatched = computed(() => {
+  if (data.value?.length > 0) {
+    let lastWatchTime = new Date(data.value[0].lastReportTime);
+    let now = new Date();
+    let timeInterval = now.getTime() - lastWatchTime.getTime();
+    if (timeInterval / 1000 <= 60) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+});
+
+setTimeout(() => {
+  if (justWatched.value) getHistory();
+}, 8000);
 </script>
