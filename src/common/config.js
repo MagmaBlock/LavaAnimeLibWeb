4b914c33c0ded60618c83e8ef0b8595a4f5f0f14
岩浆块Magma <magmablock@qq.com>
2023-10-22 22:08:29 +0800

    移除 无用配置项

--- src/common/config.js
+++ src/common/config.js
@@ -3,8 +3,5 @@ export default {
     // 定义后端的 Host
     lavaAnime: "https://anime-api.5t5.top", // 默认线上后端
     // lavaAnime: "http://127.0.0.1:8090", // 本地开发
-
-    // 定义 Bangumi API 的 Host
-    bangumi: "https://api.bgm.tv",
   },
 };
