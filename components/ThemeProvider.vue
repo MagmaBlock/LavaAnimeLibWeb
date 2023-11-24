b9e4ff2531ae170bc9f8d58e6e1c1be683e8242b
岩浆块Magma <magmablock@qq.com>
2023-11-24 11:04:38 +0800

    优化 深色模式下的 Popover 和 Modal 的颜色；新增 主题覆盖合并

--- components/ThemeProvider.vue
+++ components/ThemeProvider.vue
@@ -3,7 +3,7 @@
     :locale="zhCN"
     :date-locale="dateZhCN"
     :theme="settings.darkMode.enable ? darkTheme : null"
-    :theme-overrides="themeOverrides"
+    :theme-overrides="overrides"
   >
     <slot></slot>
   </NConfigProvider>
@@ -15,22 +15,23 @@ import { usePreferredDark } from "@vueuse/core";
 
 const settings = useSettingsStore();
 
-// 用户移动的系统深色模式设置
+// 用户的系统深色模式设置
 const isDark = usePreferredDark();
-// 当系统深色模式变化时尝试应用深色模式
+// 当系统深色模式变化时 改变深色模式的相关响应式
 watch(isDark, () => settings.applySystemDark(isDark.value), {
   immediate: true,
 });
 // 启动时尝试根据时间应用深色模式
 settings.applyTimeDark();
 
+// TailwindCSS 的深色应用
 useHead({
   htmlAttrs: {
     class: () => (settings.darkMode.enable ? "dark" : ""),
   },
 });
 
-// 覆盖默认主题配置
+// NaiveUI 主题自定义
 const themeOverrides = {
   common: {
     primaryColor: "#2080F0FF",
@@ -40,4 +41,50 @@ const themeOverrides = {
     borderRadius: "4px",
   },
 };
+
+const darkThemeOverrides = {
+  common: {
+    popoverColor: "rgba(39, 39, 42, 1)",
+    modalColor: "rgba(24, 24, 28, 1)",
+  },
+};
+
+/**
+ * 根据深色模式计算应该使用的样式覆盖
+ */
+const overrides = computed(() => {
+  if (settings.darkMode.enable) {
+    return mergeThemes(darkThemeOverrides, themeOverrides);
+  } else {
+    return themeOverrides;
+  }
+});
+
+/**
+ * 深层合并两个对象
+ * @author ChatGPT
+ * @param {...Object} themes NaiveUIThemeOverrides
+ */
+function mergeThemes(...themes) {
+  const mergedTheme = {};
+
+  themes.forEach((theme) => {
+    Object.keys(theme).forEach((key) => {
+      if (!mergedTheme[key]) {
+        mergedTheme[key] = theme[key];
+      } else {
+        if (
+          typeof mergedTheme[key] === "object" &&
+          typeof theme[key] === "object"
+        ) {
+          mergedTheme[key] = { ...mergedTheme[key], ...theme[key] };
+        } else {
+          mergedTheme[key] = theme[key];
+        }
+      }
+    });
+  });
+
+  return mergedTheme;
+}
 </script>
