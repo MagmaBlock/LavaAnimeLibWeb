70e4ee3ea17b00a032462d1346a6ddac25fe1f45
岩浆块Magma <magmablock@qq.com>
2024-02-23 17:01:22 +0800

    修复深色模式下 Markdown 颜色问题

--- components/Help/MarkdownRender.vue
+++ components/Help/MarkdownRender.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid">
     <div
-      class="place-self-center markdown-body max-w-3xl py-4 lg:px-20 dark:bg-neutral-900 dark:text-zinc-200"
+      class="place-self-center markdown-body max-w-3xl py-4 lg:px-20 dark:bg-[#101014] dark:text-zinc-200"
       v-html="html"
     ></div>
   </div>
