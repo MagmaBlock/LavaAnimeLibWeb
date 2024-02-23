78d3389a3ea4c85e30f36d7d352329a9c71df0aa
岩浆块Magma <magmablock@qq.com>
2024-02-23 23:20:48 +0800

    用户信息界面左侧菜单使用 NMenu 实现

--- layouts/user-info.vue
+++ layouts/user-info.vue
@@ -5,39 +5,42 @@
     </template>
     <ContainerPageLeftMenuRightContent>
       <template #left>
-        <NList hoverable clickable bordered>
-          <NListItem
-            v-for="menuItem in menu"
-            @click="router.replace({ path: menuItem.path })"
-          >
-            {{ menuItem.label }}
-          </NListItem>
-        </NList>
+        <NCard :bordered="false" content-style="padding: 0;">
+          <NMenu :options="options" @update:value="handlSelect" />
+        </NCard>
       </template>
       <template #right>
-        <NuxtPage class="max-w-xl"></NuxtPage>
+        <NCard :bordered="false">
+          <NuxtPage></NuxtPage>
+        </NCard>
       </template>
     </ContainerPageLeftMenuRightContent>
   </ContainerPage>
 </template>
 
-<script setup>
-const route = useRoute();
-const router = useRouter();
+<script setup lang="tsx">
+import type { MenuOption } from "naive-ui";
 
 document.title = "用户信息 | 熔岩番剧库 LavaAnimeLib";
 
-// 当打开 /user/info 时, 跳转到一个默认页
-if (route.name == "UserInfo") {
-  router.replace({ path: "/user/info/avater" });
-}
+const router = useRouter();
 
-const menu = [
-  { label: "修改头像", path: "/user/info/avatar" },
+const options: MenuOption[] = [
+  {
+    label: "修改头像",
+    key: "/user/info/avatar",
+  },
   {
     label: "修改用户名",
-    path: "/user/info/name",
+    key: "/user/info/name",
   },
-  { label: "修改密码", path: "/user/info/password" },
+  {
+    label: "修改密码",
+    key: "/user/info/password",
+  },
 ];
+
+const handlSelect = (key: string, item: MenuOption) => {
+  router.replace({ path: key });
+};
 </script>
