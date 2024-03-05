d5ec058fb1f3b9ff3db63d611fc423259ada84e8
岩浆块Magma <magmablock@qq.com>
2024-03-05 11:24:17 +0800

    尝试修复部分 Android 浏览器背景图片无法占据全屏的 BUG

--- components/DefaultLayout.vue
+++ components/DefaultLayout.vue
@@ -11,7 +11,9 @@
     </NScrollbar>
 
     <!-- 背景图片和他的特效 -->
-    <div class="absolute inset-y-0 inset-x-0 overflow-hidden -z-50">
+    <div
+      class="absolute inset-y-0 inset-x-0 h-screen w-screen overflow-hidden -z-50"
+    >
       <Transition
         enter-active-class="animate__animated animate__fadeIn animate__faster"
         leave-active-class="animate__animated animate__fadeOut animate__faster"
