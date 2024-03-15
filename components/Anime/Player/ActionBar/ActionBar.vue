f817a76f799fb373b8e7915d2ab4c74adbd91eb7
岩浆块Magma <magmablock@qq.com>
2024-03-15 14:37:07 +0800

    优化 AnimeActionBar

--- components/Anime/Player/ActionBar/ActionBar.vue
+++ components/Anime/Player/ActionBar/ActionBar.vue
@@ -1,75 +1,68 @@
 <template>
-  <AnimeCardBasic class="select-none overflow-hidden relative">
-    <!-- 未选择集数的遮罩 -->
-    <Transition name="fade">
-      <div
-        class="absolute inset-0 bg-white dark:bg-zinc-900 bg-opacity-90 dark:bg-opacity-90 z-10 grid place-items-center"
-        v-if="!store.activeFile?.url"
-      >
-        请先选择集数
-      </div>
-    </Transition>
-    <!-- 浏览器无法播放部分视频的警告 -->
-    <AnimeNoBrowserNotice v-if="store.isNoBrowser" class="mb-2" />
-    <!-- 操作栏部分 -->
-    <NScrollbar x-scrollable>
-      <div class="flex flex-nowrap flex-shrink-0 gap-1 md:gap-2">
-        <AnimePlayerActionBarIcons ref="icons">
-          <template #showAll>
-            <!-- 展开全部 -->
-            <AnimePlayerActionBarIcon
-              class="mr-2"
-              @click="
-                pausePlayer();
-                moreModel = true;
-              "
-            >
-              <i class="bi bi-three-dots"></i>
-            </AnimePlayerActionBarIcon>
+  <NCollapseTransition :show="store.activeFile?.url !== undefined">
+    <NCard size="small" :bordered="false" class="select-none overflow-hidden">
+      <!-- 浏览器无法播放部分视频的警告 -->
+      <AnimeNoBrowserNotice v-if="store.isNoBrowser" class="mb-2" />
+      <!-- 操作栏部分 -->
+      <NScrollbar x-scrollable>
+        <div class="flex flex-nowrap flex-shrink-0 gap-1 md:gap-2">
+          <AnimePlayerActionBarIcons ref="icons">
+            <template #showAll>
+              <!-- 展开全部 -->
+              <AnimePlayerActionBarIcon
+                class="mr-2"
+                @click="
+                  pausePlayer();
+                  moreModel = true;
+                "
+              >
+                <i class="bi bi-three-dots"></i>
+              </AnimePlayerActionBarIcon>
+            </template>
+          </AnimePlayerActionBarIcons>
+        </div>
+      </NScrollbar>
+      <!-- Model 模态框 -->
+      <NModal v-model:show="moreModel" class="h-fit select-none">
+        <NCard
+          class="max-w-xl"
+          title="全部播放器"
+          :bordered="false"
+          size="small"
+          role="dialog"
+          aria-modal="true"
+        >
+          <template #header-extra>
+            <i
+              class="bi bi-x-lg hover:text-blue-600 cursor-pointer ml-2"
+              @click="moreModel = false"
+            ></i>
           </template>
-        </AnimePlayerActionBarIcons>
-      </div>
-    </NScrollbar>
-    <!-- Model 模态框 -->
-    <NModal v-model:show="moreModel" class="h-fit select-none">
-      <NCard
-        class="max-w-xl"
-        title="全部播放器"
-        :bordered="false"
-        size="small"
-        role="dialog"
-        aria-modal="true"
-      >
-        <template #header-extra>
-          <i
-            class="bi bi-x-lg hover:text-blue-600 cursor-pointer ml-2"
-            @click="moreModel = false"
-          ></i>
-        </template>
-        <div class="flex flex-wrap gap-1 md:gap-2 mb-4">
-          <AnimePlayerActionBarIcons :allos="true" />
-        </div>
-        <div class="text-gray-600 dark:text-gray-400 text-xs">
-          番剧库会根据您使用的设备，判断支持的外部播放器。<br />而这里是所有设备可用的播放器，它们可能不支持您的设备。<br />
-        </div>
-        <NuxtLink
-          to="/help?article=WhyExternalPlayer"
-          class="text-blue-500 text-xs block mt-2"
-        >
-          部分视频提示 “需要外部播放器”？
-        </NuxtLink>
-        <NuxtLink
-          to="/help?article=ExternalPlayerList"
-          class="text-blue-500 text-xs block"
-        >
-          可用的外部播放器列表
-        </NuxtLink>
-        <div class="text-gray-600 dark:text-gray-400 text-xs mt-2">
-          图标绘制：Arthals
-        </div>
-      </NCard>
-    </NModal>
-  </AnimeCardBasic>
+          <div class="flex flex-wrap gap-1 md:gap-2 mb-4">
+            <AnimePlayerActionBarIcons :allos="true" />
+          </div>
+          <div class="text-gray-600 dark:text-gray-400 text-xs">
+            番剧库会根据您使用的设备，判断支持的外部播放器。<br />而这里是所有设备可用的播放器，它们可能不支持您的设备。<br />
+          </div>
+          <NuxtLink
+            to="/help?article=WhyExternalPlayer"
+            class="text-blue-500 text-xs block mt-2"
+          >
+            部分视频提示 “需要外部播放器”？
+          </NuxtLink>
+          <NuxtLink
+            to="/help?article=ExternalPlayerList"
+            class="text-blue-500 text-xs block"
+          >
+            可用的外部播放器列表
+          </NuxtLink>
+          <div class="text-gray-600 dark:text-gray-400 text-xs mt-2">
+            图标绘制：Arthals
+          </div>
+        </NCard>
+      </NModal>
+    </NCard>
+  </NCollapseTransition>
 </template>
 
 <script setup>
