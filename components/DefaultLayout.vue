6796668a9c69d476c877f365c7d5d4b6a9ffac5e
岩浆块Magma <magmablock@qq.com>
2024-08-21 15:21:39 +0800

    调整了 DefaultLayout 布局实现

--- components/DefaultLayout.vue
+++ components/DefaultLayout.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex max-w-[2560px] mx-auto">
+  <div class="fixed h-full w-full flex flex-nowrap max-w-[2560px] mx-auto">
     <!-- 导航栏 -->
-    <NavBar class="fixed h-screen z-50" />
+    <NavBar class="flex-none h-full" />
     <!-- 当前路由的界面 -->
-    <!-- 手机端: 自动添加底部 padding; 宽屏: 自动添加导航栏宽度的左 padding -->
-    <NScrollbar class="h-screen mx-auto">
-      <div class="pb-36 lg:pb-0 lg:pl-[84px]">
+    <NScrollbar class="flex-1 h-full mx-auto">
+      <!-- 手机端: 自动添加底部 padding; 宽屏: 自动添加导航栏宽度的左 padding -->
+      <div class="pb-36 lg:pb-0">
         <slot></slot>
       </div>
     </NScrollbar>
