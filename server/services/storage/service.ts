3ed553bb25217c5436d4884aba7e706bc4c90be4
岩浆块Magma <magmablock@qq.com>
2024-08-24 02:08:07 +0800

    修复 applyStorageScraperResult 在连接现有番剧的文件、站点时不处理的 BUG

--- server/services/storage/service.ts
+++ server/services/storage/service.ts
@@ -13,13 +13,15 @@ import { AlistStorageSystem } from "./system/alist";
 import type { StorageSystem } from "./system/interface";
 
 export class StorageService {
+  private readonly prisma = App.instance.prisma;
+
   /**
    * 获取 Storage 对象
    * @param storageId Storage 的 id
    * @returns Storage 对象
    */
   async getStorage(storageId: string): Promise<Storage | null> {
-    return await App.instance.prisma.storage.findUnique({
+    return await this.prisma.storage.findUnique({
       where: { id: storageId },
     });
   }
@@ -28,7 +30,7 @@ export class StorageService {
    * 获取所有 Storage
    */
   async getAllStorage(): Promise<Storage[]> {
-    const storages = await App.instance.prisma.storage.findMany();
+    const storages = await this.prisma.storage.findMany();
     return storages.filter((s) => s !== null);
   }
 
@@ -63,21 +65,22 @@ export class StorageService {
    * @param result 由 StorageScraper 挂削出的结果
    */
   async applyStorageScraperResult(result: StorageScrapeResult): Promise<void> {
-    const prisma = App.instance.prisma;
     if (result.createAnime && result.updateAnime) {
       throw new Error(
         "StorageScrapeResult 中不能同时存在 createAnime 和 updateAnime"
       );
     }
 
+    let animeId: number | undefined;
+
     if (result.createAnime) {
-      if (result.createAnime.name === undefined) {
+      if (!result.createAnime.name) {
         throw new Error(
           "StorageScrapeResult 申请创建 Anime, 缺少必选字段 name."
         );
       }
 
-      const newAnime = await prisma.anime.create({
+      const newAnime = await this.prisma.anime.create({
         data: {
           name: result.createAnime.name,
           originalName: result.createAnime.originalName,
@@ -93,99 +96,112 @@ export class StorageService {
         },
       });
 
-      if (result.connectSites) {
-        await createSitesToAnime(
-          result.connectSites.sites,
-          newAnime.id,
-          prisma
-        );
-      }
+      App.instance.logger.trace(
+        "[applyStorageScraperResult] 创建 Anime",
+        newAnime
+      );
 
-      if (result.connectFiles) {
-        await connectFilesToAnime(
-          result.connectFiles.files,
-          newAnime.id,
-          prisma
-        );
-      }
+      animeId = newAnime.id;
     }
 
     if (result.updateAnime) {
-      if (result.updateAnime.animeId === undefined) {
+      if (!result.updateAnime.animeId) {
         throw new Error(
           "StorageScrapeResult 申请更新 Anime, 但没有提供 animeId."
         );
       }
 
-      await prisma.anime.update({
+      await this.prisma.anime.update({
         where: { id: result.updateAnime.animeId },
         data: result.updateAnime.data,
       });
 
-      if (result.connectSites) {
-        await createSitesToAnime(
-          result.connectSites.sites,
-          result.updateAnime.animeId,
-          prisma
+      App.instance.logger.trace(
+        "[applyStorageScraperResult] 更新 Anime",
+        result.updateAnime
+      );
+
+      animeId = result.updateAnime.animeId;
+    }
+
+    if (result.connectSites) {
+      if (!animeId && !result.connectSites.animeId) {
+        throw new Error(
+          "StorageScrapeResult 申请更新 AnimeSiteLink, 但没有提供 animeId."
         );
       }
 
-      if (result.connectFiles) {
-        await connectFilesToAnime(
-          result.connectFiles.files,
-          result.updateAnime.animeId,
-          prisma
+      await this.createSitesToAnime(
+        result.connectSites.sites,
+        animeId || result.connectSites.animeId!
+      );
+    }
+
+    if (result.connectFiles) {
+      if (!animeId && !result.connectFiles.animeId) {
+        throw new Error(
+          "StorageScrapeResult 申请更新 AnimeFileLink, 但没有提供 animeId."
         );
       }
+
+      await this.connectFilesToAnime(
+        result.connectFiles.files,
+        animeId || result.connectFiles.animeId!
+      );
     }
+  }
 
-    async function createSitesToAnime(
-      sites: Partial<AnimeSiteLink>[],
-      animeId: number,
-      prisma: PrismaClient
-    ) {
-      const connectSites = sites.map((s) => {
-        if (s.siteType === undefined || s.siteId === undefined) {
-          throw new Error(
-            "StorageScrapeResult 申请创建 AnimeSiteLink, 但甚至没有提供 siteType 或 siteId."
-          );
-        }
-        return {
-          siteType: s.siteType,
-          siteId: s.siteId,
-        };
-      });
+  private async createSitesToAnime(
+    sites: Partial<AnimeSiteLink>[],
+    animeId: number
+  ) {
+    const connectSites = sites.map((s) => {
+      if (!s.siteType || !s.siteId) {
+        throw new Error(
+          "StorageScrapeResult 申请创建 AnimeSiteLink, 但甚至没有提供 siteType 或 siteId."
+        );
+      }
+      return {
+        siteType: s.siteType,
+        siteId: s.siteId,
+      };
+    });
 
-      await prisma.anime.update({
-        where: { id: animeId },
-        data: {
-          sites: {
-            createMany: {
-              data: connectSites,
-            },
+    await this.prisma.anime.update({
+      where: { id: animeId },
+      data: {
+        sites: {
+          createMany: {
+            data: connectSites,
           },
         },
-      });
-    }
+      },
+    });
 
-    async function connectFilesToAnime(
-      files: StorageIndex[],
-      animeId: number,
-      prisma: PrismaClient
-    ) {
-      const connectFiles = files.map((f) => {
-        return { id: f.id };
-      });
+    App.instance.logger.trace(
+      "[applyStorageScraperResult] 更新 AnimeSiteLink",
+      connectSites
+    );
+  }
 
-      await prisma.anime.update({
-        where: { id: animeId },
-        data: {
-          files: {
-            connect: connectFiles,
-          },
+  private async connectFilesToAnime(files: StorageIndex[], animeId: number) {
+    const connectFiles = files.map((f) => {
+      return { id: f.id };
+    });
+
+    await this.prisma.anime.update({
+      where: { id: animeId },
+      data: {
+        files: {
+          connect: connectFiles,
         },
-      });
-    }
+      },
+    });
+
+    App.instance.logger.trace(
+      "[applyStorageScraperResult] 更新 AnimeFileLink",
+      connectFiles
+    );
   }
 
   /**
