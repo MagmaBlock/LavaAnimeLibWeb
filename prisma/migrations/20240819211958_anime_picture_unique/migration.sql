9585ce88de89be17a31375c44aac03b8af9aaed7
岩浆块Magma <magmablock@qq.com>
2024-08-28 01:49:46 +0800

    移除 开发阶段的各种 prisma migration；AnimeViewHistory 支持 removed 字段

--- prisma/migrations/20240819211958_anime_picture_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[type,animeId]` on the table `AnimePicture` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `AnimePicture_type_animeId_key` ON `AnimePicture`(`type`, `animeId`);
