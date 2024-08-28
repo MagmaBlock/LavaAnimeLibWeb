8d93869d1278fcf6fbe6386ead3192b942b6c935
岩浆块Magma <magmablock@qq.com>
2024-08-29 01:42:04 +0800

    弃用 dayjs

--- app.vue
+++ app.vue
@@ -20,23 +20,6 @@ useHead({
   },
 });
 
-import dayjs from "dayjs";
-import "dayjs/locale/zh-cn";
-import updateLocale from "dayjs/plugin/updateLocale";
-
-dayjs.locale("zh-cn");
-dayjs.extend(updateLocale);
-dayjs.updateLocale("zh-cn", {
-  calendar: {
-    lastDay: "[昨天] HH:mm",
-    sameDay: "[今天] HH:mm",
-    nextDay: "[明天] HH:mm",
-    lastWeek: "[上周]dd HH:mm",
-    nextWeek: "[下周]dd HH:mm",
-    sameElse: "YYYY-MM-DD HH:mm",
-  },
-});
-
 const pageLifeCycleStore = usePageLifeCycleStore();
 pageLifeCycleStore.mountTime = new Date();
 window.addEventListener("click", () => {
