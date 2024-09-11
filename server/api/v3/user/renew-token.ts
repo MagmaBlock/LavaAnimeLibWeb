623173dd722c55e4efe96fd32a79966ffde5ce10
岩浆块Magma <magmablock@qq.com>
2024-09-11 16:46:48 +0800

    删除 V3早期编写的部分测试 HTTP API

--- server/api/v3/user/renew-token.ts
+++
@@ -1,8 +0,0 @@
-import { App } from "~/server/services/app";
-import { UserService } from "~/server/services/user/service";
-
-export default defineEventHandler(async (event) => {
-  const userService = App.instance.services.getService(UserService);
-  const user = await userService.assertUser(event);
-  return { token: userService.signTokenByUserId(user.id) };
-});
