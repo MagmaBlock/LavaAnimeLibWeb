3802c382fda063c01591047498d3e6702364de91
岩浆块Magma <magmablock@qq.com>
2024-09-18 23:50:03 +0800

    优化 个人界面功能图标

--- components/User/FunctionsCard/FunctionsCard.vue
+++ components/User/FunctionsCard/FunctionsCard.vue
@@ -2,37 +2,32 @@
   <NCard :bordered="false" embedded>
     <!-- Contents -->
     <div class="flex-1 self-center grid grid-cols-4 sm:grid-cols-4 gap-2">
-      <!-- 个人设置 -->
+      <!-- 用户设置 -->
       <UserFunctionsCardBigButton @click="router.push('/user/setting/avatar')">
         <template #icon="iconProps">
-          <Icon name="material-symbols:account-box" :size="iconProps.size" />
+          <Icon name="material-symbols:checkbook" :size="iconProps.size" />
         </template>
-        <template #name>个人信息</template>
-      </UserFunctionsCardBigButton>
-      <!-- 帮助 -->
-      <UserFunctionsCardBigButton @click="router.push('/help')">
-        <template #icon="iconProps">
-          <Icon
-            name="material-symbols:library-books-outline"
-            :size="iconProps.size"
-          />
-        </template>
-        <template #name>使用帮助</template>
+        <template #name>用户设置</template>
       </UserFunctionsCardBigButton>
       <!-- 深色模式 -->
       <UserFunctionsCardBigButton @click="showDarkModeModel = true">
         <template #icon="iconProps">
-          <Icon
-            name="material-symbols:dark-mode-outline"
-            :size="iconProps.size"
-          />
+          <Icon name="material-symbols:dark-mode" :size="iconProps.size" />
         </template>
         <template #name>深色模式</template>
       </UserFunctionsCardBigButton>
       <UserFunctionsCardDarkModeModel v-model:show="showDarkModeModel" />
+      <!-- 使用帮助 -->
+      <UserFunctionsCardBigButton @click="router.push('/help')">
+        <template #icon="iconProps">
+          <Icon name="material-symbols:help" :size="iconProps.size" />
+        </template>
+        <template #name>使用帮助</template>
+      </UserFunctionsCardBigButton>
+      <!-- 更新日志 -->
       <UserFunctionsCardBigButton @click="router.push('/about/update')">
         <template #icon="iconProps">
-          <Icon name="material-symbols:history" :size="iconProps.size" />
+          <Icon name="material-symbols:edit-document" :size="iconProps.size" />
         </template>
         <template #name>更新日志</template>
       </UserFunctionsCardBigButton>
