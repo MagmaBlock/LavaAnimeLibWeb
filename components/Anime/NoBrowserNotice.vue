1fe6c3b177ff6f803d4c0c7215dd0f63df46227e
岩浆块Magma <magmablock@qq.com>
2024-11-09 19:04:02 +0800

    优化 浏览器兼容不佳的视频的提示文本

--- components/Anime/NoBrowserNotice.vue
+++ components/Anime/NoBrowserNotice.vue
@@ -2,8 +2,8 @@
   <div class="flex space-x-3 items-center">
     <Icon size="32" name="material-symbols:error" />
     <div class="text-xs">
-      此视频很可能无法在浏览器中播放，画面、声音或字幕均可能异常。<br />
-      如您不能正常观看请使用下方第三方播放器或下载后进行播放。
+      如果您发现此视频无声/画面异常/无字幕/无法播放，可能是当前浏览器无法支持。<br />
+      如您不能正常观看请使用下方第三方播放器、更换浏览器或下载后进行播放。
     </div>
   </div>
 </template>
