1ba9146755aeaeb8716f3923fcd1380d8826b1fd
岩浆块Magma <magmablock@qq.com>
2024-11-11 16:53:47 +0800

    优化 NoBrowserNotice 组件样式及文案

--- components/Anime/NoBrowserNotice.vue
+++ components/Anime/NoBrowserNotice.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex space-x-3 items-center">
-    <Icon size="32" name="material-symbols:error" />
+  <div class="flex gap-x-3 items-center">
+    <Icon size="32" name="material-symbols:error" class="hidden lg:block" />
     <div class="text-xs">
-      如果您发现此视频无声/画面异常/无字幕/无法播放，可能是当前浏览器无法支持。<br />
-      如您不能正常观看请使用下方第三方播放器、更换浏览器或下载后进行播放。
+      因兼容原因，此视频可能无声/无画面/无字幕/无法播放。<br />
+      如不能正常观看请使用下方播放器、更换浏览器或下载后进行播放。
     </div>
   </div>
 </template>
