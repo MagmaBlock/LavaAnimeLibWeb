6469c308c946b6b905d24b3b1917281ec84e9974
岩浆块Magma <magmablock@qq.com>
2024-11-12 04:08:28 +0800

    尝试实现 ArtPlayer ass 字幕播放，可能失败

--- components/Anime/Player/Player.vue
+++ components/Anime/Player/Player.vue
@@ -12,6 +12,7 @@
 <script setup>
 import Artplayer from "artplayer";
 import { useLocalStorage, useThrottleFn } from "@vueuse/core";
+import artplayerPluginLibass from "artplayer-plugin-libass";
 
 const store = useAnimeStore();
 const message = useMessage();
@@ -75,6 +76,16 @@ onMounted(() => {
           if (newEp) store.changeEpisode(newEp);
         }, 1000),
       },
+    ],
+    plugins: [
+      artplayerPluginLibass({
+        debug: true,
+        workerUrl:
+          "https://unpkg.com/libass-wasm@4.1.0/dist/js/subtitles-octopus-worker.js",
+        wasmUrl:
+          "https://unpkg.com/libass-wasm@4.1.0/dist/js/subtitles-octopus-worker.wasm",
+        fallbackFont: "/player/SourceHanSansCN-Bold.woff2",
+      }),
     ],
   };
 
