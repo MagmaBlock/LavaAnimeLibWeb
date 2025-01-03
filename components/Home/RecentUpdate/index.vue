830e26275d836c35929f542620a8a7bbf9ab215a
岩浆块Magma <magmablock@qq.com>
2025-01-04 02:42:54 +0800

    feat(Home/RecentUpdate): 优化近期更新组件
    - 添加显示/隐藏重复集数功能
    - 优化卡片布局和样式
    - 增加骨架屏和错误处理

--- components/Home/RecentUpdate/index.vue
+++ components/Home/RecentUpdate/index.vue
@@ -1,10 +1,17 @@
 <template>
   <NCard title="近期更新" embedded :bordered="false">
+    <template #header-extra>
+      <NFlex class="cursor-pointer" @click="messageSkiped = !messageSkiped">
+        <NSwitch :value="messageSkiped" />
+        显示重复集数
+      </NFlex>
+    </template>
     <div
-      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4"
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
     >
       <!-- 每个更新卡片 -->
       <NuxtLink
+        v-if="!loading"
         v-for="record in data"
         :class="record.animeID ? '' : 'cursor-not-allowed'"
         :to="
@@ -16,64 +23,97 @@
               }
             : {}
         "
-        class="grid grid-cols-3 place-items-start rounded-md overflow-hidden bg-white dark:bg-zinc-950 hover:bg-zinc-100 dark:hover:bg-zinc-800"
       >
-        <div class="col-span-1 w-full h-full">
-          <div class="aspect-w-2 aspect-h-3">
-            <img
-              v-if="record.anime?.images?.poster"
-              :src="record.anime?.images?.poster"
-              class="object-cover"
-            />
+        <NCard
+          content-style="padding: 0px"
+          class="overflow-hidden hover:bg-zinc-100 dark:hover:bg-zinc-800 h-full"
+        >
+          <div class="grid grid-cols-3 h-full">
+            <!-- 图片 -->
             <div
-              v-else
-              class="w-full h-full bg-zinc-200 dark:bg-zinc-800"
-            ></div>
-          </div>
-        </div>
-        <div class="col-span-2 w-full h-full px-3 py-1.5 relative">
-          <!-- title -->
-          <div class="text-base">
-            <NEllipsis :line-clamp="2" v-if="record.anime">
-              {{ record.anime.title }}
-            </NEllipsis>
-            <NEllipsis :line-clamp="2" v-else>
-              {{ record.index?.split("/")?.[2] }}
-            </NEllipsis>
-          </div>
-          <!-- time -->
-          <div class="text-xs opacity-75">
-            <span>{{ getTimeInfo(record.uploadTime) }}</span>
-            <span class="mx-1.5 font-bold">·</span>
-            <!-- index -->
-            <span v-if="record.anime">
-              {{ record.anime.index.year }}{{ record.anime.index.type }}
-            </span>
-            <span v-else>
-              {{ record.index?.split("/")?.[0]
-              }}{{ record.index?.split("/")?.[1] }}
-            </span>
-          </div>
-          <!-- groups / episode -->
-          <NSpace :size="4" class="my-2 absolute bottom-0">
-            <NTag
-              v-if="record.parseResult?.episode"
-              size="small"
-              type="primary"
-              :bordered="false"
+              v-if="record.anime?.images?.poster"
+              class="col-span-1 w-full h-full"
             >
-              第 {{ record.parseResult?.episode }} 话
-            </NTag>
-            <NTag
-              v-for="group in record.parseResult?.groups"
-              size="small"
-              :bordered="false"
+              <div class="aspect-w-2 aspect-h-3">
+                <img
+                  :src="record.anime?.images?.poster"
+                  class="object-cover"
+                  v-lazy
+                />
+              </div>
+            </div>
+            <!-- 右侧信息 -->
+            <NFlex
+              vertical
+              justify="space-between"
+              :class="
+                record.anime?.images?.poster ? 'col-span-2' : 'col-span-3'
+              "
+              class="w-full h-full px-3 pt-1.5 pb-2"
             >
-              {{ group.result }}
-            </NTag>
-          </NSpace>
-        </div>
+              <!-- title -->
+              <div>
+                <!-- 正常标题 -->
+                <NEllipsis :line-clamp="1" v-if="record.anime">
+                  {{ record.anime.title }}
+                </NEllipsis>
+                <!-- 无 anime 标题 (文件夹名) -->
+                <NEllipsis :line-clamp="1" v-else>
+                  {{ record.index?.split("/")?.[2] }}
+                </NEllipsis>
+              </div>
+
+              <div>
+                <!-- episode -->
+                <div
+                  v-if="record.parseResult?.episode"
+                  class="flex place-items-center gap-1 text-xs opacity-75"
+                >
+                  <NIcon>
+                    <Icon name="material-symbols:stack" />
+                  </NIcon>
+                  第 {{ record.parseResult?.episode }} 话
+                </div>
+
+                <!-- index -->
+                <div class="flex place-items-center gap-1 text-xs opacity-75">
+                  <Icon name="material-symbols:calendar-month" />
+                  <div v-if="record.anime">
+                    {{ record.anime.index.year }}{{ record.anime.index.type }}
+                  </div>
+                  <div v-else>
+                    {{ record.index?.split("/")?.[0]
+                    }}{{ record.index?.split("/")?.[1] }}
+                  </div>
+                </div>
+
+                <!-- groups -->
+                <div
+                  v-if="record.parseResult?.groups?.length"
+                  class="flex place-items-center gap-1 text-xs opacity-75"
+                >
+                  <NIcon>
+                    <Icon name="material-symbols:group" />
+                  </NIcon>
+                  <div>
+                    <template v-for="group in record.parseResult?.groups">
+                      {{ group.result }}
+                    </template>
+                  </div>
+                </div>
+
+                <!-- time -->
+                <div class="flex place-items-center gap-1 text-xs opacity-75">
+                  <Icon name="material-symbols:schedule" />
+                  <span>{{ getTimeInfo(record.uploadTime) }}</span>
+                </div>
+              </div>
+            </NFlex>
+          </div>
+        </NCard>
       </NuxtLink>
+
+      <NSkeleton v-if="loading" v-for="a in 20" class="aspect-w-2 aspect-h-1" />
     </div>
   </NCard>
 </template>
@@ -82,72 +122,41 @@
 import moment from "moment";
 import "moment/dist/locale/zh-cn";
 
-const data = ref([]);
+const message = useMessage();
 
-try {
-  let result = await LavaAnimeAPI.get("/v2/anime/recent-update/get", {
-    params: {
-      take: 36,
-    },
-  });
-  if (result.data.code === 200) {
-    data.value = result.data.data;
-  }
-} catch (error) {}
+const data = ref([]);
+const messageSkiped = ref(false);
+const loading = ref(true);
 
 const getTimeInfo = (dateTime) => {
   return moment(dateTime).locale("zh-cn").calendar();
 };
 
-// interface RecentUpdate {
-//   id: number;
-//   animeID: number;
-//   bangumiID: number;
-//   fileName: string;
-//   index: string;
-//   messageSentStatus: boolean;
-//   uploadTime: string;
-//   parseResult: {
-//     animeTitle: string | null,
-//     animeYear: string | null,
-//     episode: string | null,
-//     extensionName: string | null,
-//     fileName: string,
-//     groups: {
-//       result?: string,
-//       raw: string,
-//       type: "group",
-//     }[],
-//     noBrowser: boolean,
-//     otherInfo: {
-//       result?: string,
-//       raw: string,
-//       type: string,
-//     }[],
-//     tagedName:
-//       | {
-//           result?: string,
-//           raw: string,
-//           type: string,
-//         }[]
-//       | string,
-//     videoQuality: {
-//       result: string,
-//       raw: string,
-//       type: "quality",
-//     }[],
-//     videoSource: {
-//       result: string,
-//       raw: string,
-//       type: "source",
-//     }[],
-//     videoSubtitle: {
-//       result: string,
-//       raw: string,
-//       type: "subtitle",
-//     }[],
-//   };
-// }
+getRecentUpdates();
+
+watch(messageSkiped, () => {
+  getRecentUpdates();
+});
+
+async function getRecentUpdates() {
+  loading.value = true;
+  try {
+    let result = await LavaAnimeAPI.get("/v2/anime/recent-update/get", {
+      params: {
+        take: 36,
+        messageSkiped: messageSkiped.value,
+      },
+    });
+
+    if (result.data.code === 200) {
+      data.value = result.data.data;
+    }
+  } catch (error) {
+    message.error("获取近期更新列表失败");
+    console.error(error);
+  }
+  loading.value = false;
+}
 </script>
 
 <style></style>
