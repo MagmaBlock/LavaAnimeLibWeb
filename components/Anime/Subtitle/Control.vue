1d36512cb75925fc31851c02829a59f0bf42b9ae
岩浆块Magma <magmablock@qq.com>
2025-04-08 01:35:26 +0800

    refactor(Anime): 优化字幕选择界面布局和功能
    - 调整字幕选择界面布局，防止换行导致的显示问题
    - 添加对长字幕名称的处理，避免溢出
    - 在 NSelect 组件中添加下拉箭头和点击触发
    - 简化字幕选择提示信息

--- components/Anime/Subtitle/Control.vue
+++ components/Anime/Subtitle/Control.vue
@@ -1,25 +1,23 @@
 <template>
   <NCollapseTransition :show="haveSubtitles">
-    <div class="flex flex-col gap-2">
-      <div class="flex items-center gap-2">
-        <NSwitch v-model:value="animeStore.subtitleData.enabled" />
-        <Icon name="material-symbols:subtitles-outline" :size="24" />
-        <NSelect
-          size="small"
-          :options="subtitleSelectList"
-          :value="subtitleSelectCurrentValue"
-          @update:value="handleSubtitleSelect"
-        >
-          <template #header>
-            <NAlert type="info">
-              本视频支持外挂字幕，您可以切换不同的字幕轨道。通常中文字幕以
-              "zh"、"chi" 来结尾。
-              <br />
-              番剧库默认会尝试帮您选择中文字幕，但这并不总是完美。
-            </NAlert>
-          </template>
-        </NSelect>
-      </div>
+    <div class="flex items-center gap-2 flex-nowrap">
+      <NSwitch v-model:value="animeStore.subtitleData.enabled" />
+      <Icon name="material-symbols:subtitles-outline" class="w-6 h-6" />
+      <NSelect
+        v-if="haveSubtitles"
+        size="small"
+        :options="subtitleSelectList"
+        :value="subtitleSelectCurrentValue"
+        :dropdown-props="{ trigger: 'click', showArrow: true }"
+        @update:value="handleSubtitleSelect"
+        class="truncate"
+      >
+        <template #header>
+          <NAlert type="info">
+            本视频支持外挂字幕，您可以切换不同的字幕轨道。通常中文字幕以"zh"、"chi"来结尾。
+          </NAlert>
+        </template>
+      </NSelect>
     </div>
   </NCollapseTransition>
 </template>
@@ -33,8 +31,11 @@ const haveSubtitles = computed(() =>
 
 const subtitleSelectList = computed(() => {
   return animeStore.subtitleList.map((file) => {
+    const displayName =
+      file.name.length > 60 ? `...${file.name.slice(-30)}` : file.name;
+
     return {
-      label: file.name,
+      label: displayName,
       value: file.name,
     };
   });
@@ -49,5 +50,3 @@ function handleSubtitleSelect(value: string) {
   animeStore.subtitleData.enabled = true;
 }
 </script>
-
-<style></style>
